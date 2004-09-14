90c58a9fc2e4486ac283793fbf50a63ee4a13d67
Eric Gruson <eric.gruson@polytechnique.org>
2004-09-14 22:53:42 +0000

    Amelioration bis du login

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,5 +1,21 @@
 <?php
 require "include/global.inc.php";
 demande_authentification(AUTH_MDP);
-rediriger_vers("/");
+
+// Génération de la page
+require_once BASE_LOCAL."/include/page_header.inc.php";
+
+echo "<page id='accueil' titre='Frankiz : accueil'>\n";
+?>
+
+
+<h2>Tu es bien authentifié !</h2>
+
+<p>&nbsp;</p>
+<p>Te voilà prêt à accéder au fabuleux monde du campus de l'X</p>
+<p>Si tu veux éviter de te ré-identifier à chaque fois que tu accèdes à cette page, alors vas dans tes <a href="profil/profil.php">profils</a> et selectionne l'authentification par cookie</p>
+<p>Sinon navigue sur la page en tout tranquilité ...</p>
+<?
+echo "</page>\n";
+require_once BASE_LOCAL."/include/page_footer.inc.php";
 ?>
