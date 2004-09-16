96ea50c178de2956b1d3222559dd30eb75bbaeb0
Eric Gruson <eric.gruson@polytechnique.org>
2004-09-16 15:22:51 +0000

    Rajout de la ligne qui va bien pour les parametres (pour ne pas perdre de page d'administration ca serait balot)

--- htdocs/admin/index.php
+++ htdocs/admin/index.php
@@ -4,9 +4,12 @@
 	l'utilisateur courant à accès.
 
 	$Log$
+	Revision 1.10  2004/09/16 15:22:51  kikx
+	Rajout de la ligne qui va bien pour les parametres (pour ne pas perdre de page d'administration ca serait balot)
+
 	Revision 1.9  2004/09/15 23:20:18  schmurtz
 	Suppression de la variable CVS "Id" (fait double emploi avec "Log")
-
+	
 	Revision 1.8  2004/09/15 21:42:27  schmurtz
 	Commentaires et ajout de la variable cvs "Log"
 	
@@ -29,6 +32,7 @@ require_once BASE_LOCAL."/include/page_header.inc.php";
 		<lien titre="Liste des sections" url="<?php echo BASE_URL?>/admin/sections.php"/>
 		<lien titre="Liste des IPs" url="<?php echo BASE_URL?>/admin/ip.php"/>
 		<lien titre="Gerer les demandes d'ajout d'ips" url="<?php echo BASE_URL?>/admin/valid_ip.php"/>
+		<lien titre="Changer les variables globales" url="<?php echo BASE_URL?>/admin/parametre.php"/>
 	<h2>Webmestre binet X</h2>
 	<p>En construction
 </p>
