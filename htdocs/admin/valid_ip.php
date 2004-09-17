70f81a98926ea684628a6d08c2996ee7f451b9c3
Eric Gruson <eric.gruson@polytechnique.org>
2004-09-17 12:45:22 +0000

    Permet de voi quel sont les ips que la personne a d�j� avant de valider ... en particulier ca permet de pas se planter de sous r�seau !!!!!!!!!!!!!

--- htdocs/admin/valid_ip.php
+++ htdocs/admin/valid_ip.php
@@ -5,8 +5,8 @@
 	ou refuse la demande ici.
 	
 	$Log$
-	Revision 1.12  2004/09/17 11:55:03  kikx
-	Bug Fix dans la page de la validation d'ip ...
+	Revision 1.13  2004/09/17 12:45:22  kikx
+	Permet de voi quel sont les ips que la personne a déjà avant de valider ... en particulier ca permet de pas se planter de sous réseau !!!!!!!!!!!!!
 
 	Revision 1.10  2004/09/15 23:20:18  schmurtz
 	Suppression de la variable CVS "Id" (fait double emploi avec "Log")
@@ -49,13 +49,13 @@ foreach ($_POST AS $keys => $val){
 					"\n" .
 					"Très Cordialement\n" .
 					"Le BR\n"  ;
-		
+				
 		$DB_trombino->query("SELECT login,nom,prenom,mail FROM eleves WHERE eleve_id='{$temp[1]}'");
 		list($login,$nom,$prenom,$mail) = $DB_trombino->next_row() ;
 		if (($mail=="")||($mail=="NULL")) $mail = $login."@poly.polytechnique.fr" ;
 	
 		mail("\"$prenom $nom\" <$mail>","[Frankiz] Ta demande a été refusée ",$contenu);
-
+		echo "<warning><p>Envoie d'un mail à $mail</p><p>Le prévient que sa demande n'est pas acceptée</p></warning>" ;
 	}
 	// On accepte la demande d'ip supplémentaire
 	//===========================
@@ -65,21 +65,30 @@ foreach ($_POST AS $keys => $val){
 		$DB_trombino->query("SELECT piece_id FROM eleves WHERE eleve_id='{$temp[1]}'") ;
 		list($kzert) = $DB_trombino->next_row();
 		
-		$DB_valid->query("DELETE FROM valid_ip WHERE eleve_id='{$temp[1]}'");
-		$DB_admin->query("INSERT prises SET prise_id='',piece_id='$kzert',ip='{$_POST[$temp2]}',type='secondaire'");
+		$DB_admin->query("SELECT 0 FROM prises WHERE ip='{$_POST[$temp2]}'");
 		
-		$contenu = "Bonjour, \n\n".
-					"Nous t'avons ouvert l'ip suivante :\n".
-					$_POST[$temp2]."\n".
-					"\n" .
-					"Très Cordialement\n" .
-					"Le BR\n"  ;
+		// S'il n'y a aucune entrée avec cette ip dans la base
+		if ($DB_admin->num_rows()==0){
+			$DB_valid->query("DELETE FROM valid_ip WHERE eleve_id='{$temp[1]}'");
+			$DB_admin->query("INSERT prises SET prise_id='',piece_id='$kzert',ip='{$_POST[$temp2]}',type='secondaire'");
+			
+			$contenu = "Bonjour, \n\n".
+						"Nous t'avons ouvert l'ip suivante :\n".
+						$_POST[$temp2]."\n".
+						"\n" .
+						"Très Cordialement\n" .
+						"Le BR\n"  ;
+			
+			$DB_trombino->query("SELECT login,nom,prenom,mail FROM eleves WHERE eleve_id='{$temp[1]}'");
+			list($login,$nom,$prenom,$mail) = $DB_trombino->next_row();
+			if (($mail=="")||($mail=="NULL")) $mail = $login."@poly.polytechnique.fr" ;
 		
-		$DB_trombino->query("SELECT login,nom,prenom,mail FROM eleves WHERE eleve_id='{$temp[1]}'");
-		list($login,$nom,$prenom,$mail) = $DB_trombino->next_row();
-		if (($mail=="")||($mail=="NULL")) $mail = $login."@poly.polytechnique.fr" ;
-	
-		mail("\"$prenom $nom\" <$mail>","[Frankiz] Ta demande a été acceptée",$contenu);
+			mail("\"$prenom $nom\" <$mail>","[Frankiz] Ta demande a été acceptée",$contenu);
+			echo "<warning><p>Envoie d'un mail à $mail</p><p>Le prévient que sa demande à été accepté (Nlle ip =".$_POST[$temp2].") </p></warning>" ;
+		// S'il y  a deja une entrée comme celle demandé dans la base !
+		} else {
+			echo "<warning><p>IMPOSSIBLE DE METTRE CETTE IP</p><p>Il y a déjà une autre personne la possédant</p></warning>" ;		
+		}
 
 	}
 	
@@ -96,11 +105,12 @@ foreach ($_POST AS $keys => $val){
 					"Très Cordialement\n" .
 					"Le BR\n"  ;
 		
-		$DB_trombino->query("SELECT login,nom,prenom,mail FROM eleves WHERE eleve_id='{$temp[1]}'");
+		$DB_trombino->query("SELECT login,nom,prenom,mail FROM eleves WHERE eleve_id='{$temp[2]}'");
 		list($login,$nom,$prenom,$mail) = $DB_trombino->next_row() ;
 		if (($mail=="")||($mail=="NULL")) $mail = $login."@poly.polytechnique.fr" ;
 	
 		mail("\"$prenom $nom\" <$mail>","[Frankiz] Suppression d'une ip",$contenu);
+		echo "<warning><p>Envoie d'un mail à $mail</p><p>Le previent que son ip $temp2 vient d'être supprimé</p></warning>" ;			
 
 	}
 }
@@ -113,7 +123,6 @@ Vous allez valider un ajout d'une ip : Pour le moment le système n'est pas fiabl
 	<liste id="liste" selectionnable="non" action="admin/valid_ip.php">
 		<entete id="eleve" titre="Élève"/>
 		<entete id="raison" titre="Raison"/>
-		<entete id="prises" titre="Prises"/>
 		<entete id="ip" titre="IP"/>
 <?
 		$DB_valid->query("SELECT v.raison,e.nom,e.prenom,e.piece_id,e.eleve_id FROM valid_ip as v INNER JOIN trombino.eleves as e USING(eleve_id)");
@@ -124,12 +133,20 @@ Vous allez valider un ajout d'une ip : Pour le moment le système n'est pas fiabl
 				<colonne id="raison">
 					<zonetext titre="" id="raison_<? echo $eleve_id ;?>" valeur="<? echo $raison ;?>"/>
 				</colonne>
-				<colonne id="prises">
-				</colonne>
 				<colonne id="ip">
-					<champ titre="" id="ajout_ip_<? echo $eleve_id ;?>" valeur="129.104." /> 
-					<bouton titre="Ok" id="ok_<? echo $eleve_id ;?>"/>
-					<bouton titre="Vtff" id="vtff_<? echo $eleve_id ;?>"/>
+<?
+					$DB_admin->query("SELECT ip FROM prises WHERE piece_id='$piece'") ;
+					while(list($ip)=$DB_admin->next_row()) {
+						echo "<p>" ;
+							echo $ip ;
+						echo "</p>" ;
+					}
+?>					
+					<p>
+						<champ titre="" id="ajout_ip_<? echo $eleve_id ;?>" valeur="129.104." /> 
+						<bouton titre="Ok" id="ok_<? echo $eleve_id ;?>" />
+						<bouton titre="Vtff" id="vtff_<? echo $eleve_id ;?>" onClick="return window.confirm('Voulez vous vraiment ne pas valider cette ip ?')"/>
+					</p>
 				</colonne>
 			</element>
 <?
@@ -142,16 +159,14 @@ Vous allez valider un ajout d'une ip : Pour le moment le système n'est pas fiabl
 	<h2>Liste des personnes ayant eu leurs ips supplémentaires</h2>
 	<liste id="liste" selectionnable="non" action="admin/valid_ip.php">
 		<entete id="eleve" titre="Élève"/>
-		<entete id="prise" titre="Prise"/>
 		<entete id="ip" titre="IP"/>
 <?
-		$DB_admin->query("SELECT e.nom,e.prenom,prises.prise_id,prises.ip FROM prises INNER JOIN trombino.eleves as e USING(piece_id) WHERE type='secondaire' ORDER BY e.nom ASC, e.prenom ASC");
-		while(list($nom,$prenom,$prise,$ip) = $DB_admin->next_row()) {
+		$DB_admin->query("SELECT e.eleve_id,e.nom,e.prenom,prises.ip FROM prises INNER JOIN trombino.eleves as e USING(piece_id) WHERE type='secondaire' ORDER BY e.nom ASC, e.prenom ASC");
+		while(list($id,$nom,$prenom,$ip) = $DB_admin->next_row()) {
 ?>
 			<element id="<? echo str_replace(".","x",$ip) ;?>">
 				<colonne id="eleve"><? echo "$nom $prenom" ?></colonne>
-				<colonne id="prise"><? echo $prise ?></colonne>
-				<colonne id="ip"><? echo $ip ;?><bouton titre="Dégage!" id="suppr_<? echo str_replace(".","x",$ip) ;?>"/></colonne>
+				<colonne id="ip"><? echo $ip ;?><bouton titre="Dégage!" id="suppr_<? echo str_replace(".","x",$ip) ;?>_<? echo $id?>" onClick="return window.confirm('Voulez vous vraiment supprimez cette ip ?')"/></colonne>
 			</element>
 <?
 		}
