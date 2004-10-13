03d6e773cc8047e203e91434f1f4c41c3f8e057e
Guillaume Porcher <guillaume.porcher@polytechnique.org>
2004-10-13 20:03:59 +0000

    Ajout du lien

--- htdocs/modules/liens_contacts.php
+++ htdocs/modules/liens_contacts.php
@@ -3,11 +3,14 @@
 	Liens permettants de contacter les webmestres et faire des demandes.
 	
 	$Log$
+	Revision 1.10  2004/10/13 20:03:59  pico
+	Ajout du lien
+
 	Revision 1.9  2004/10/06 14:12:27  kikx
 	Page de mail promo quasiment en place ...
 	envoie en HTML ...
 	Page pas tout a fait fonctionnel pour l'instant
-
+	
 	Revision 1.8  2004/09/20 22:19:28  kikx
 	test
 	
@@ -35,6 +38,7 @@
 	<?php if(est_authentifie(AUTH_MINIMUM)): ?>
 		<lien titre="Proposer une annonce" url="proposition/annonce.php" />
 		<lien titre="Proposer une activité" url="proposition/affiche.php" />
+		<lien titre="Proposer une qdj" url="proposition/qdj.php" />
 		<lien titre="Demander un mail promo" url="proposition/mail_promo.php" />
 		<lien titre="Proposer un sondage" url="proposition/sondage.php/" />
 	<?php endif; ?>
