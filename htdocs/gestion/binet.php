e317b58d2967090ad07622b6ada93f5002eac076
Eric Gruson <eric.gruson@polytechnique.org>
2004-10-17 17:31:32 +0000

    Micro modif avant que j'oublie

--- htdocs/gestion/binet.php
+++ htdocs/gestion/binet.php
@@ -7,10 +7,13 @@
 	L'ID du binet à administrer est passer dans le paramètre GET 'binet'.
 	
 	$Log$
+	Revision 1.2  2004/10/17 17:31:32  kikx
+	Micro modif avant que j'oublie
+
 	Revision 1.1  2004/10/17 15:22:05  kikx
 	Mise en place d'un repertoire de gestion qui se différencie de admin car ce n'est pas l'admin :)
 	En gros il servira a tout les modification des prez des webmestres , des pages persos, ...
-
+	
 	Revision 1.4  2004/09/15 23:20:18  schmurtz
 	Suppression de la variable CVS "Id" (fait double emploi avec "Log")
 	
@@ -24,9 +27,10 @@ require_once "../include/global.inc.php";
 
 // Vérification des droits
 demande_authentification(AUTH_FORT);
-if(empty($_GET['binet']) || !verifie_permission_webmestre($_GET['binet']) || !verifie_permission_prez($_GET['binet']))
+if ((empty($_GET['binet'])) || ((!verifie_permission_webmestre($_GET['binet'])) && (!verifie_permission_prez($_GET['binet']))))
 	rediriger_vers("/admin/");
-$est_prez = verifie_permission_prez($_GET['binet']);
+echo $est_prez = verifie_permission_prez($_GET['binet']);
+
 
 // Génération de la page
 require_once BASE_LOCAL."/include/page_header.inc.php";
