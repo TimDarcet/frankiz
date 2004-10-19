f6d1e786377d390f66ebde0f2b9389e497b877f4
Guillaume Porcher <guillaume.porcher@polytechnique.org>
2004-10-19 12:34:15 +0000

    Inclusion de l'�tat des serveurs d'apr�s la sortie du script

--- htdocs/modules/stats.php
+++ htdocs/modules/stats.php
@@ -11,9 +11,12 @@
 	de bande passante, de cpu). 
 	
 	$Log$
+	Revision 1.6  2004/10/19 12:34:15  pico
+	Inclusion de l'état des serveurs d'après la sortie du script
+
 	Revision 1.5  2004/09/15 23:20:29  schmurtz
 	Suppression de la variable CVS "Id" (fait double emploi avec "Log")
-
+	
 	Revision 1.4  2004/09/15 21:42:32  schmurtz
 	Commentaires et ajout de la variable cvs "Log"
 	
@@ -21,16 +24,11 @@
 if(est_authentifie(AUTH_MINIMUM)) { ?>
 	<module id="stats" titre="Statistiques">
 		<statistiques>
-			<serveur nom="frankiz" etat="up" uptime="<?php @include 'http://frankiz.polytechnique.fr/uptimefkz' ?>" />
-			<serveur nom="gwennoz" etat="up" uptime="<?php @include 'http://gwennoz.polytechnique.fr/uptimegwz' ?>" />
-			<serveur nom="heol" etat="up" uptime="<?php @include 'http://heol.polytechnique.fr/uptimeheol' ?>" />
 			<service nom="web frankiz" stat="http://frankiz.polytechnique.fr/webalizer/" />
 			<service nom="web binets" stat="http://gwennoz.polytechnique.fr/webalizer/" />
 			<service nom="news" stat="http://frankiz.polytechnique.fr/news/" />
 			<service nom="xnet" stat="http://frankiz.polytechnique.fr/accueil/xnetstatquick.php" />
-			<serveur nom="kuzh" etat="down" />
-			<serveur nom="sil" etat="up" />
-			<serveur nom="poly" etat="up" />
+		<? @include BASE_CACHE."status" ?>
 		</statistiques>
 	</module>
 <?php } ?>
