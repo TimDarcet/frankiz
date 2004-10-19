33183ed990f5c573f054f8c433bc111abdfae95b
Guillaume Porcher <guillaume.porcher@polytechnique.org>
2004-10-19 22:14:49 +0000

    Suppression du lien corrige ton poly Redirection des mails que si authentifi�

--- htdocs/modules/liens_ecole.php
+++ htdocs/modules/liens_ecole.php
@@ -3,9 +3,13 @@
 	Liens permettants d'accéder aux autres sites de l'école.
 	
 	$Log$
+	Revision 1.4  2004/10/19 22:14:49  pico
+	Suppression du lien corrige ton poly
+	Redirection des mails que si authentifié
+
 	Revision 1.3  2004/09/15 23:20:29  schmurtz
 	Suppression de la variable CVS "Id" (fait double emploi avec "Log")
-
+	
 	Revision 1.2  2004/09/15 21:42:32  schmurtz
 	Commentaires et ajout de la variable cvs "Log"
 	
@@ -15,8 +19,7 @@
 	<lien titre="La Kes" url="http://binets.polytechnique.fr/kes/" />
 	<lien titre="Aide aux binets" url="http://binets.polytechnique.fr/kes-binets/" />
 	<lien titre="Le site des élèves" url="http://www.polytechnique.fr/eleves/" />
-	<lien titre="Redirection des mails" url="http://poly.polytechnique.fr/" />
-	<lien titre="Corrige ton poly" url="http://binets.polytechnique.fr/corrigepoly/" />
+	<?php if(est_authentifie(AUTH_MINIMUM)){ ?><lien titre="Redirection des mails" url="http://poly.polytechnique.fr/" /> <? } ?>
 	<lien titre="Site de l'école" url="http://www.polytechnique.fr/" />
 	<lien titre="Site de la DE" url="http://www.edu.polytechnique.fr/" />
 	<lien titre="Bureau des effectifs" url="http://intranet-effectifs.polytechnique.fr/" />
