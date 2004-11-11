e2e6f6c59de75c86dfdcc5f2a3a4e63b3341353f
Eric Gruson <eric.gruson@polytechnique.org>
2004-11-11 17:42:26 +0000

    pour avoir la liste des binets

---
+++ htdocs/admin/binets_liste.php
@@ -0,0 +1,99 @@
+<?php
+/*
+	Copyright (C) 2004 Binet Réseau
+	http://www.polytechnique.fr/eleves/binets/br/
+	
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+/*
+	Gestion de la liste des binets.
+
+	$Log$
+	Revision 1.1  2004/11/11 17:42:26  kikx
+	pour avoir la liste des binets
+
+
+	
+	
+*/
+
+// En-tetes
+require_once "../include/global.inc.php";
+
+// Vérification des droits
+demande_authentification(AUTH_FORT);
+if(!verifie_permission('admin'))
+	rediriger_vers("/admin/");
+
+$message = "";
+$texte_image ="" ;
+// =====================================
+// Modification d'un binet
+// =====================================
+
+	// On crée un binet
+	//==========================
+
+	if (isset($_POST['ajout'])) {
+		$DB_trombino->query("INSERT INTO binets SET nom='{$_POST['nom']}'");
+		$index = mysql_insert_id() ;
+		$message .= "<commentaire>Création du binet ' {$_POST['nom']}' effectuée</commentaire>" ;
+	}
+	
+
+// Génération de la page
+require_once BASE_LOCAL."/include/page_header.inc.php";
+?>
+<page id="binets_web" titre="Frankiz : Binets Web">
+<? echo $message ;?>
+
+<h1>Liste des Binets</h1>
+
+	<?
+	$categorie_precedente = -1;
+	
+	$DB_trombino->query("SELECT nom,binet_id FROM binets ORDER BY nom ASC");
+	?>
+	<liste id='liste_binet' titre='Liste de binets' selectionnable='non'>
+		<entete id="login" titre="Login"/>
+	<?
+		while(list($nom_binet,$binet_id) = $DB_trombino->next_row()) {
+		?>
+			<element id="<?=$binet_id?>">
+				<colonne id="login">
+				<lien titre="<?=$nom_binet?>" url="admin/binets.php?id=<?=$binet_id?>"/>
+				</colonne>
+			</element>
+		<?
+		}
+	?>	
+	</liste>
+
+
+
+<h1>Création d'un binet</h1>
+
+		<formulaire id="binet_web" titre="Nouveau Binet" action="admin/binets_liste.php">
+			<hidden id="id" titre="ID" valeur=""/>
+			<champ id="nom" titre="Nom" valeur=""/>
+			<bouton id='ajout' titre="Ajouter"/>
+		</formulaire>
+		
+
+</page>
+<?php
+require_once BASE_LOCAL."/include/page_footer.inc.php";
+?>
+
