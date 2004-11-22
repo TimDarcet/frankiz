679bea7dfd65f464d5d37136f1bc8545b0f14ab1
Guillaume Porcher <guillaume.porcher@polytechnique.org>
2004-11-22 22:41:44 +0000

    Il parait qu'on est presque � 300000 lignes !!!
    Mais en fait non, pas du tout...

--- htdocs/rss.php
+++ htdocs/rss.php
@@ -13,9 +13,12 @@ require_once BASE_LOCAL."/include/rss_func.inc.php";
 <page id="rss" titre="Frankiz : News Externes">
 
 <?
-rss_xml('http://www.liberation.fr/rss.php');
-rss_xml('http://linuxfr.org/backend/news/rss20.rss');
+rss_xml('http://www.liberation.fr/rss.php','sommaire');
+rss_xml('http://linuxfr.org/backend/news/rss20.rss','sommaire');
 rss_xml('http://linuxfr.org/backend/news-homepage/rss20.rss','sommaire');
+rss_xml('http://www.framasoft.net/backend.php3','sommaire');
+rss_xml('http://www.infos-du-net.com/backend.php','sommaire');
+rss_xml('http://www.clubic.com/c/xml.php?type=news','sommaire');
 ?>
 </page>
 <?
