bed7b44fb6950b1d6497bda1b5ee8f8c53fe8331
Guillaume Porcher <guillaume.porcher@polytechnique.org>
2004-11-23 21:05:47 +0000

    pr�paration passage liste des rss par variable de session

--- htdocs/rss.php
+++ htdocs/rss.php
@@ -13,20 +13,30 @@ require_once BASE_LOCAL."/include/rss_func.inc.php";
 <page id="rss" titre="Frankiz : News Externes">
 
 <?
-rss_xml('http://www.liberation.fr/rss.php','sommaire');
-rss_xml('http://linuxfr.org/backend/news/rss20.rss','sommaire');
-rss_xml('http://linuxfr.org/backend/news-homepage/rss20.rss','sommaire');
-rss_xml('http://www.framasoft.net/backend.php3','sommaire');
-rss_xml('http://www.infos-du-net.com/backend.php','sommaire');
-rss_xml('http://www.clubic.com/c/xml.php?type=news','sommaire');
-rss_xml('http://hyperlinkextractor.free.fr/rssfiles/google_france.xml','sommaire');
-rss_xml('http://www.humanite.fr/backend_une.php3','sommaire');
-rss_xml('http://www.lexpress.fr/getfeedrss.asp','sommaire');
-rss_xml('http://permanent.nouvelobs.com/cgi/rss/permanent_une','sommaire');
-rss_xml('http://www.vnunet.fr/rssrdf/news.xml ','sommaire');
-rss_xml('http://www.microsite.reuters.com/rss/topNews','sommaire');
-rss_xml('http://www.washingtonpost.com/wp-srv/world/rssheadlines.xml','sommaire');
-rss_xml('http://mozillazine.org/contents.rdf','sommaire');
+$array = array(
+	'http://www.liberation.fr/rss.php'=>'complet',
+	'http://linuxfr.org/backend/news/rss20.rss'=>'sommaire',
+	'http://linuxfr.org/backend/news-homepage/rss20.rss'=>'sommaire',
+	'http://www.framasoft.net/backend.php3'=>'sommaire',
+	'http://www.infos-du-net.com/backend.php'=>'sommaire',
+	'http://www.clubic.com/c/xml.php?type=news'=>'sommaire',
+	'http://hyperlinkextractor.free.fr/rssfiles/google_france.xml'=>'sommaire',
+	'http://www.humanite.fr/backend_une.php3'=>'sommaire',
+	'http://www.lexpress.fr/getfeedrss.asp'=>'sommaire',
+	'http://permanent.nouvelobs.com/cgi/rss/permanent_une'=>'sommaire',
+	'http://www.vnunet.fr/rssrdf/news.xml '=>'sommaire',
+	'http://www.microsite.reuters.com/rss/topNews'=>'sommaire',
+	'http://www.washingtonpost.com/wp-srv/world/rssheadlines.xml'=>'sommaire',
+	'http://mozillazine.org/contents.rdf'=>'sommaire',
+	);
+
+$_SESSION['rss'] = base64_encode(serialize($array));
+
+$liens = unserialize(base64_decode($_SESSION['rss']));
+foreach($liens as $value => $mode){
+	rss_xml($value,$mode);
+}
+
 ?>
 </page>
 <?
