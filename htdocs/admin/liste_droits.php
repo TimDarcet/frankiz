a974d4d354849fd54ddb9424f076e61523551ab3
Guillaume Porcher <guillaume.porcher@polytechnique.org>
2005-01-18 13:31:21 +0000

    Ajout liste des webmestres et des prez

--- htdocs/admin/liste_droits.php
+++ htdocs/admin/liste_droits.php
@@ -20,9 +20,12 @@
 /*
 	
 	$Log$
+	Revision 1.5  2005/01/18 13:31:21  pico
+	Ajout liste des webmestres et des prez
+
 	Revision 1.4  2005/01/03 23:27:05  pico
 	Petites modifs
-
+	
 	Revision 1.3  2005/01/03 20:05:07  pico
 	Corrections diverses
 	
@@ -60,6 +63,31 @@ foreach(liste_droits() as $droit => $description){
 	}
 }
 
+	// Pour les webmestres des binets
+	echo "<h3>Binets: Webmestres</h3>";
+	$DB_trombino->query("SELECT nom,binet_id FROM binets");
+	while(list($binet,$id) = $DB_trombino->next_row()){
+		$DB_trombino->push_result();
+		$DB_trombino->query("SELECT eleves.eleve_id,eleves.nom,prenom,surnom FROM eleves LEFT JOIN frankiz2.compte_frankiz AS cpt ON eleves.eleve_id=cpt.eleve_id WHERE  cpt.perms LIKE '%webmestre_{$id},%' ORDER BY eleves.nom,prenom ASC");
+		while(list($eleve_id,$nom,$prenom,$surnom) = $DB_trombino->next_row()) {
+			echo "$binet: $nom $prenom ($surnom)\n";
+			echo "<lien url='".BASE_URL."/admin/user.php?id=$eleve_id' titre='Administrer'/><br/>\n" ;
+		}
+		$DB_trombino->pop_result();
+	}
+	
+	// Pour les prez des binets
+	echo "<h3>Binets: Prez</h3>";
+	$DB_trombino->query("SELECT nom,binet_id FROM binets");
+	while(list($binet,$id) = $DB_trombino->next_row()){
+		$DB_trombino->push_result();
+		$DB_trombino->query("SELECT eleves.eleve_id,eleves.nom,prenom,surnom FROM eleves LEFT JOIN frankiz2.compte_frankiz AS cpt ON eleves.eleve_id=cpt.eleve_id WHERE  cpt.perms LIKE '%prez_{$id},%' ORDER BY eleves.nom,prenom ASC");
+		while(list($eleve_id,$nom,$prenom,$surnom) = $DB_trombino->next_row()) {
+			echo "$binet: $nom $prenom ($surnom)\n";
+			echo "<lien url='".BASE_URL."/admin/user.php?id=$eleve_id' titre='Administrer'/><br/>\n" ;
+		}
+		$DB_trombino->pop_result();
+	}
 ?>
 
 </page>
