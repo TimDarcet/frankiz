8ebb95e01680d37828a83181750e7d0c59c3460c
Guillaume Porcher <guillaume.porcher@polytechnique.org>
2005-02-04 17:36:35 +0000

    ce sera pt�t mieux comme �a

--- htdocs/admin/histo_qdj.php
+++ htdocs/admin/histo_qdj.php
@@ -21,9 +21,12 @@
 	Page qui permet aux admins de voir l'historique des qdj
 	
 	$Log$
+	Revision 1.8  2005/02/04 17:36:35  pico
+	ce sera ptèt mieux comme ça
+
 	Revision 1.7  2005/02/04 17:32:26  pico
 	Correction bug division par zero
-
+	
 	Revision 1.6  2005/01/06 23:31:31  pico
 	La QDJ change à 0h00 (ce n'est plus la question du jour plus un petit peu)
 	
@@ -72,7 +75,7 @@ $date = date("Y-m-d", time());
 	<h4>Nous sommes le : <?= date("d/m/Y",strtotime($date)) ?></h4>
 	<h2>Historique</h2>
 	<?
-	$DB_web->query("SELECT qdj_id,date,question,reponse1,reponse2,compte1,compte2 FROM qdj WHERE date<'$date'  ORDER BY date DESC");
+	$DB_web->query("SELECT qdj_id,date,question,reponse1,reponse2,compte1,compte2 FROM qdj WHERE date!='0000-00-00' AND date<'$date'  ORDER BY date DESC");
 	while(list($id,$date,$question,$reponse1,$reponse2,$compte1,$compte2) = $DB_web->next_row()){
 		if(($compte1+$compte2)!=0){ 
 			$p1 = round(100*$compte1/($compte1+$compte2));
