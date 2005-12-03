f15f3d9b5ca5572cf383653b75c5e07f63a951de
Vincent Zanotti <vincent.zanotti@polytechnique.org>
2005-12-03 22:51:53 +0000

    Suppression des mots de passes dans les scripts perl de frankiz. Création d'un fichier de conf contenant ces mots de passe.

--- bin/xnet.pl
+++ bin/xnet.pl
@@ -3,10 +3,15 @@
 #Script pour les stats du nb de connectés xnet
 # Garde 144 résultats, soit 6* 24: Une journée avec un enregistrement toutes les 10minutes.
 
+$script_dir = `/bin/dirname $0`;
+$script_dir =~ s/\n//gs;
+require "$script_dir/../etc/config.pl";
+
 $file="/home/frankiz2/cache/stats-xnet";
 use DBI();
 
-my $dbh = DBI->connect("DBI:mysql:database=xnet:host=heol","web","kokouije?.",{'RaiseError'=>1});
+my $dbh = DBI->connect("DBI:mysql:database=$mysql_xnet{database}:host=$mysql_xnet{host}",
+	"$mysql_xnet{user}","$mysql_xnet{password}",{'RaiseError'=>1});
 
 $reqt="select sum(isconnected) as 'Connectés' from clients";
 my $rep = $dbh->prepare($reqt);
---
+++ etc/config.defaut.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+# Base du serveur xNet
+%mysql_xnet = (
+	host		=> '',
+	database	=> '',
+	user		=> '',
+	password	=> ''
+);
+
+# Base du site frankiz
+%mysql_frankiz2 = (
+	host		=> '',
+	database	=> '',
+	user		=> '',
+	password	=> ''
+);
