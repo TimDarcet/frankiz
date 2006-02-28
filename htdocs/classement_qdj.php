1861f6818883a90bbb1ae34a4cb58efc6602e7b0
Alexandre Pilkiewicz <alexandre.pilkiewicz@polytechnique.org>
2006-02-28 08:34:36 +0000

    Fin des changements d affichage de la QDJ. C est laid pour l ecart type, mais j ai pas reussi a faire mieux

--- htdocs/classement_qdj.php
+++ htdocs/classement_qdj.php
@@ -27,8 +27,8 @@ demande_authentification(AUTH_INTERNE);
 
 if(isset($_REQUEST["graph"])){
 	header ("Content-type: image/png");
-	$nom=array(" ","1er","2e","3e","13","42","69","pi","ip","bonus");
-	for($i=1; $i<10; $i++){
+	$nom=array(" ","1er","2e","3e","13","42","69","pi","ip","bonus","qdj");
+	for($i=1; $i<11; $i++){
 		$nb[$nom[$i]] = (isset($_REQUEST["nb$i"])?$_REQUEST["nb$i"]:0);
 	}
 	// on calcule le nombre de pages vues sur l'année
@@ -43,12 +43,19 @@ if(isset($_REQUEST["graph"])){
 	$bleu_fonce = ImageColorAllocate ($im, 75, 130, 195);
 	$bleu_clair = ImageColorAllocate ($im, 95, 160, 240);
 	// on dessine un trait horizontal pour représenter l'axe du temps     
-	ImageLine ($im, 10, $hauteur-20, $largeur-10, $hauteur-20, $noir);
+	ImageLine ($im, 10, $hauteur-20, $largeur, $hauteur-20, $noir);
 	// on affiche le numéro des règles
 	$i=0;
 	foreach ($nb as $nom => $nombre) {
 		$i++;
-		ImageString ($im, 2, $i*$largeur/(count($nb)+1), $hauteur-18, $nom, $noir);
+		if(strlen($nom) >3){
+			$decalage = (strlen($nom) - 3) * 4;
+		}else{
+			$decalage = 0;
+		}
+			
+		ImageString ($im, 2, $i*$largeur/(count($nb)+1) - $decalage, $hauteur-18, $nom, $noir);
+		//$i++;
 //		ImageString ($im, 2, $i*$largeur/(count($nb)+1), $hauteur-18, '('.$nombre.')', $noir);
 	}
 	$i=0;
@@ -68,6 +75,7 @@ if(isset($_REQUEST["graph"])){
 			else{
 				ImageString($im, 2, $i*$largeur/(count($nb)+1)-1, $hauteur-$hauteurImageRectangle-20-12, $nombre, $noir);
 			}
+			//$i++;
 	}
 	// on dessine le tout
 	imagecolortransparent ($im,$blanc);
@@ -93,19 +101,19 @@ if(isset($_REQUEST["graph"])){
 			<li>Voter treizième vous fait perdre 13 points, (C'est mal d'essayer de prendre l'ip de la passerelle !)</li>
 			<li>Règle bonus qui rapporte 7 points au réveil !</li>
 			<li>Enfin, proposer une "bonne" QDJ, c'est-à-dire que le QDJMaster acceptera de passer, rapporte 7.1 points le jour où elle passe (utilisez votre cerveau pour battre ceux qui utilisent des scripts)</li></ul>
-			 Amusez-vous bien et surtout, lisez-la QDJ avant de voter.
+			 Amusez-vous bien et surtout, lisez la QDJ avant de voter.
 		
 		</commentaire>
 		
 		<liste id="liste_qdj" selectionnable="non">
 				<entete id="nom" titre="Nom"/>
-				<entete id="total" titre="Total"/>
+				<entete id="total" titre="Total (moyenne, écart type)"/>
 				<entete id="detail" titre="Détail"/>
 		<?
 		$DB_web->query("
 					SELECT
 						t.eleve_id, t.nom, t.prenom, t.surnom, t.promo,
-						p.total, p.nb1, p.nb2, p.nb3, p.nb4, p.nb5, p.nb6, p.nb7, p.nb8, p.nb9
+						p.total, p.nb1, p.nb2, p.nb3, p.nb4, p.nb5, p.nb6, p.nb7, p.nb8, p.nb9, p.nb10
 					FROM
 						qdj_points AS p
 					LEFT JOIN
@@ -122,12 +130,16 @@ if(isset($_REQUEST["graph"])){
 										ORDER BY te.promo DESC
 										LIMIT 0,1)
 					ORDER BY p.total DESC");
-
-		while(list($eleve_id,$nom,$prenom,$surnom,$promo,$total,$nb1,$nb2,$nb3,$nb4,$nb5,$nb6,$nb7,$nb8,$nb9) = $DB_web->next_row()) {
+		$moy = 0;
+		$ecartype = 0;
+		while(list($eleve_id,$nom,$prenom,$surnom,$promo,$total,$nb1,$nb2,$nb3,$nb4,$nb5,$nb6,$nb7,$nb8,$nb9,$nb10) = $DB_web->next_row()) {
+			$moy = ($nb1 + $nb2 + $nb3 + $nb4 + $nb5 + $nb6 + $nb7 + $nb8 + $nb9 + $nb10)/10;
+			$ecartype =sqrt(($nb1^2 + $nb2^2 + $nb3^2 + $nb4^2 + $nb5^2 + $nb6^2 + $nb7^2 + $nb8^2 + $nb9^2 + $nb10^2)/10 - $moy^2);
+			$ecartype = round($ecartype, 2);
 			echo "\t\t<element id=\"$eleve_id\">\n";
 				echo "\t\t\t<colonne id=\"nom\">".($surnom!="" ?$surnom:$nom." ".$prenom)." (X$promo)</colonne>\n";
-				echo "\t\t\t<colonne id=\"total\">$total</colonne>\n";
-				echo "\t\t\t<colonne id=\"detail\"><image source=\"classement_qdj.php?graph&amp;nb1=$nb1&amp;nb2=$nb2&amp;nb3=$nb3&amp;nb4=$nb4&amp;nb5=$nb5&amp;nb6=$nb6&amp;nb7=$nb7&amp;nb8=$nb8&amp;nb9=$nb9\" texte=\"image\"/></colonne>\n";
+				echo "\t\t\t<colonne id=\"total\">$total ($moy, $ecartype)</colonne>\n";
+				echo "\t\t\t<colonne id=\"detail\"><image source=\"classement_qdj.php?graph&amp;nb1=$nb1&amp;nb2=$nb2&amp;nb3=$nb3&amp;nb4=$nb4&amp;nb5=$nb5&amp;nb6=$nb6&amp;nb7=$nb7&amp;nb8=$nb8&amp;nb9=$nb9&amp;nb10=$nb10\" texte=\"image\"/></colonne>\n";
 			echo "\t\t</element>\n";
 		}
 		?>
