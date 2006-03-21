173df6ece0c81c39e44f3ad972c06e4c7e01252c
Fabien Laborde <fabien.laborde@polytechnique.org>
2006-03-21 14:32:54 +0000

    Import des revisions 1644 a 1646 et 1648 (fautes d'orthographes, classement qdj qui marche, liens utiles internes/externes)

--- htdocs/admin/parametre.php
+++ htdocs/admin/parametre.php
@@ -58,12 +58,12 @@ require_once BASE_LOCAL."/include/page_header.inc.php";
 <page id="admin_parametre" titre="Frankiz : Modifier les paramètres globaux">
 
 <note>
-Cette page est une page pour pouvoir modifier directement les paramètres du sites : si vous
-devez créer des varibles dans la table essayer qu'elles soient assez explicite.
+Vous pouvez ici modifier directement la configuration générale du site.
+Si vous devez créer des variables dans la table, essayez de faire qu'elles soient suffisamment explicites.
 </note>
 <h2>Paramètres du site</h2>
 	<liste id="liste" selectionnable="non" action="admin/parametre.php">
-		<entete id="nom_var" titre="Nom de la varible"/>
+		<entete id="nom_var" titre="Nom de la variable"/>
 		<entete id="valeur" titre="Valeur"/>
 <?
 		$DB_web->query("SELECT nom,valeur FROM parametres ORDER by nom");
