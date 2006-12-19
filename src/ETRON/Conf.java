a5bfe0c5f7c0507b5bd9344c49129d784c12fdfa
Alexandre Pilkiewicz <alexandre.pilkiewicz@polytechnique.org>
2006-12-19 20:03:39 +0000

    ajout d'un repertoire src a la svn pour les projet non php
    ajout des sources de l'Element Telematique de Rappel de l'Ouverture et de Notification

---
+++ src/ETRON/Conf.java
@@ -0,0 +1,24 @@
+/*
+        Copyright (C) 2006 Binet Réseau
+        http://www.polytechnique.fr/eleves/binets/br/
+
+        This program is free software; you can redistribute it and/or
+        modify it under the terms of the GNU General Public License
+        as published by the Free Software Foundation; either version 2
+        of the License, or (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+public class Conf{
+    public static String mdpBob = "le bob rend alcoolique";
+    public static String racine = "http://gwennoz.eleves.polytechnique.fr/~pilki/frankiz/htdocs/gestion/bob/";
+    public static int intervalContactable = 10000;
+    public static int intervalIncontactable = 10000;
+}
