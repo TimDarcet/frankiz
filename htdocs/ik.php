4a6831ae2e87c617601202d43ba3a039fd97a53e
Rémi Bonnet <remi.bonnet.2005@polytechnique.org>
2007-09-21 15:00:44 +0000

    Import des revisions 1945 et 1951 (acces du lien ik depuis l'exterieur)

--- htdocs/ik.php
+++ htdocs/ik.php
@@ -1,42 +1,47 @@
 <?php
 /*
-	Copyright (C) 2006 Binet Réseau
+	Copyright (C) 2004 Binet Réseau
 	http://www.polytechnique.fr/eleves/binets/br/
-	
+
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
 	as published by the Free Software Foundation; either version 2
 	of the License, or (at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
+/*
+	Recherche dans le trombino.
 
-if(est_authentifie(AUTH_INTERNE)) {
-?>
-<module id="lienik" titre="IK électronique">
-<?php
-	if(!cache_recuperer('lienik',0)) {
-		$DB_web->query("SELECT valeur FROM parametres WHERE nom='lienik'");
-		list($lienik) = $DB_web->next_row();
-		$lienik_full = "ik.php?id=".$lienik;
+	$Id: trombino.php 1943 2007-09-13 21:44:09Z elscouta $
 
-		echo "<a href=\"$lienik_full\"><image source=\"".BASE_URL."/data/ik_thumbnails/$lienik.png\" texte=\"IK de la semaine\"/></a> \n";
+*/
 
-		cache_sauver('lienik');
-	}
-	if (est_interne()) {
-		echo "<a href='".URL_BINETS."ik/'>Archive de l'IK</a>";
-	}
-?>
-</module>
-<?php
+require_once "include/global.inc.php";
+
+demande_authentification(AUTH_INTERNE);
+
+// Recuperation d'une image
+if (!empty($_GET['id']))
+{
+	// les magic quotes, c'est pas cool
+	$id = html_entity_decode(stripslashes($_GET['id']), ENT_QUOTES);
+	// &apos; n'est pas traduit par html_entity_decode
+	$id = str_replace("&apos;", "'", $id);
+	
+	$file = BASE_BINETS."ik/".basename($id); 
+
+	if (return_file($file))
+		exit;
 }
-?>
 
+not_found();
+
+?>
