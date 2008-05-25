5eb067b8962ae37a12a8595c4c0dd32f036dfead
Bernardo Da Costa <bernardo.da-costa@polytechnique.org>
2008-05-25 17:40:39 +0000

    Version "frankiz" pour la gestion robotran  - Skinning (include des global, page_header, page_footer, ...)  - Version "DTD" des Ã©lÃ©ments HTML (formulaire, warning, ...)  - Conversion eleve_id -> forlife@x.edu automatique si une session est prÃ©sente (attention ordis bars ?)  - Tests pour isset($var) avant $var != "" (peut-Ãªtre Ã  virer les != "" si on fait attention, mais autant Ãªtre sÃ»r)  - Corrections orthographiques
    A faire
     - Lien dans la barre de navigation
     - Deplacer index.php et new_account.php vers profil/robotran.php et profil/compte_robotran.php (?)

---
+++ htdocs/robotran/index.php
@@ -0,0 +1,126 @@
+<?php
+
+require_once "../include/global.inc.php";
+require_once "../include/wiki.inc.php";
+
+demande_authentification(AUTH_INTERNE);
+
+$not_authed = true;
+
+require_once "./robotran_mysql.php";
+// Login (sans session frankiz)
+if(isset($_POST['forlife']) && isset($_POST['passwd_robotran'])){
+	$forlife=$_POST['forlife'];
+	$mdp=$_POST['passwd_robotran'];
+	$req="SELECT mdp FROM robotran.auth WHERE forlife=\"".$forlife."\" AND mdp='".md5($mdp)."';";
+	$res=mysql_query($req);
+	if(mysql_num_rows($res)!=1){
+		$errmsg = "Erreur de connexion. Le couple identifiant/mot de passe ne correspond pas.";
+	}
+	else{
+		$not_authed = false;
+	}
+}
+// Demande de nouveau code
+else if(isset($_SESSION['forlife']) && isset($_POST['batiment'])){
+	$not_authed = false;
+	$bat=$_POST['batiment'];
+	$forlife=$_SESSION['forlife'];
+	$libre = "SELECT code FROM robotran.codes WHERE forlife IS NULL AND batiment = \"".$bat."\";";
+	$verif="SELECT code FROM robotran.codes WHERE forlife=\"".$forlife."\" AND timestamp >= (NOW() - INTERVAL 1 WEEK);";
+	$getcode="UPDATE robotran.codes SET forlife=\"".$forlife."\",timestamp=now() WHERE batiment=\"".$bat."\" AND forlife IS NULL LIMIT 1;";
+	if($bat==''){
+		$errdemande = "Il faut que tu choisisses un groupe de machines à laver.";
+	}
+	else{
+		$res=mysql_query($libre);
+		if(mysql_num_rows($res)==0){
+			$errdemande="Il n'y a plus de code dans notre base pour le groupe de machines demandé.";
+		}
+		else{
+			$res=mysql_query($verif);
+			if(mysql_num_rows($res)>=4){
+				$errdemande = "Tu as déjà demandé 4 codes pour les machines à laver cette semaine ! Pour en avoir plus, entrer en contact avec <a href=\"mailto:robotran@frankiz.polytechnique.fr\">robotran@frankiz.polytechnique.fr</a> en justifiant le besoin de plus de 4 codes.";
+			}
+			else{
+				$res=mysql_query($getcode);
+			}
+		}
+	}
+}
+// Login (session frankiz)
+if(isset($_SESSION['user'])) {
+  $eleve_id = $_SESSION['user']->uid;
+  $polyedu = "SELECT polyedu FROM trombino.eleves WHERE eleve_id = $eleve_id";
+  $res = mysql_query($polyedu);
+  if (mysql_num_rows($res)==1) {
+    $row = mysql_fetch_array($res, MYSQL_ASSOC);
+    $forlife = $row['polyedu'];
+    $not_authed = false;
+  }
+}
+
+
+require BASE_LOCAL."/include/page_header.inc.php";
+?><page id='robotran' titre='Frankiz : Codes Robotran'>
+<?php
+
+if($not_authed){
+?>
+<?php
+if( isset($errmsg) && $errmsg != ""){
+	echo("<warning>$errmsg</warning>\n");
+}
+?>
+
+<h1>Authentification requise</h1>
+Pour accéder à la page demandée, tu dois t'identifier.
+Pour cela, indique ton adresse mail polytechnique.edu (de la forme prenom.nom) et ton mot de passe pour ce site.
+
+<p>Si celle-là est ta première connexion, tu peux créer ton compte <a href="robotran/new_account.php">ici</a>.</p>
+
+<note>Ton identifiant est la partie de ton adresse mail qui vient avant @polytechnique.edu. (En général, prenom.nom)</note>
+<formulaire id="login_robotran" titre="Connexion" action="">
+	<champ id="forlife" titre="Identifiant" valeur="<?php if(isset($_POST['forlife'])) echo $_POST['forlife']?>"/>
+	<champ id="passwd_robotran" titre="Mot de passe" valeur=""/>
+	<bouton id="connect" titre="Continuer"/>
+</formulaire>
+
+<?php
+}else{
+	$_SESSION['forlife']=$forlife;
+?>
+<h1>Tu es maintenant authentifié en tant que <?php echo "$forlife"?>.</h1>
+
+<h2>Liste de codes dont tu disposes déjà:</h2>
+<?php
+require_once "./robotran_mysql.php";
+$lst="SELECT code,batiment FROM robotran.codes WHERE forlife = \"".$forlife."\" ORDER BY timestamp;";
+$res=mysql_query($lst);
+while ($row = mysql_fetch_array($res, MYSQL_ASSOC)) {
+	$code=$row['code'];
+	$bat=$row['batiment'];
+	echo "<p>*".$code."# (".$bat.")</p>";
+}
+?>
+<formulaire id="forlife" titre="Demande de nouveau code" action="">
+<?php
+if(isset($errdemande) && $errdemande != ""){
+	echo("<warning>$errdemande</warning>\n");
+}
+?>
+  <choix titre="Batiment" id="batiment" type="combo" valeur="<?php echo empty($_REQUEST['batiment']) ? '' : $_REQUEST['batiment']; ?>">
+    <option titre="   " id=""/>
+    <option titre="Fayolle" id="Fayolle"/>
+    <option titre="PEM" id="PEM"/>
+  </choix>
+  <bouton id='envoyer' titre='Demander'/>
+</formulaire>
+<?php
+}
+?>
+</page>
+<?php
+require BASE_LOCAL."/include/page_footer.inc.php";
+
+?>
---
+++ htdocs/robotran/new_account.php
@@ -0,0 +1,83 @@
+<?php
+
+function mdp($length = 8)
+{
+    $carac = 'AZERTYUIOPMLKJHGFDSQWXCVBNazertyuiopmlkjhgfdsqwxcvbn1234567890';
+    $mdp   = '';
+    for($i = 0 ; $i < $length ; $i++) {
+        $mdp .= $carac[mt_rand(0, strlen($carac) - 1)];
+    }
+    return $mdp;
+
+}
+
+function sendmdp($forlife,$mdp){
+require_once "../htdocs/include/mail.inc.php";
+        $mail = new Mail( "Codes Robotran <robotran@frankiz.polytechnique.fr>"  , $forlife."@polytechnique.edu" , "Ton nouveau mot de passe pour demander des codes pour robotran",false,"", "robotran@frankiz.polytechnique.fr");
+        $mail->setBody("Bonjour,\n\nTon nouveau mot de passe pour le site de distribution des codes d'acces aux machines à laver robotran (http://frankiz.polytechnique.fr/robotran/) est ".$mdp."\n\nTrès cordialement,\n--\nLe BR (pour la distribution des codes robotran)");
+        $mail->send();
+}
+
+
+require_once "../include/global.inc.php";
+require_once "../include/wiki.inc.php";
+
+demande_authentification(AUTH_INTERNE);
+
+
+if(isset($_POST['forlife'])){
+require_once "./robotran_mysql.php";
+	$forlife=$_POST['forlife'];
+	$mdp=mdp();
+	$req="SELECT mdp FROM auth WHERE forlife='$forlife';";
+	$res=mysql_query($req);
+	if(mysql_num_rows($res)==0){
+		$ins="INSERT INTO auth (forlife,mdp) VALUES (\"$forlife\",\"".md5($mdp)."\");";
+		$res=mysql_query($ins);
+		if($res){
+			sendmdp($forlife,$mdp);
+			$msg = "Compte créé avec succès. Un mail a été envoyé à ton adresse $forlife@polytechnique.edu avec ton mot de passe.";
+		}
+		else{
+			$msg = "Un problème est survenu et ton compte n'a pas pu être créé. Contacte un <a href=\"mailto:robotran@frankiz.polytechnique.fr\">administrateur du système</a> ";
+		}
+	}else{
+		$ins="UPDATE auth SET mdp = \"".md5($mdp)."\" WHERE forlife=\"$forlife\";";
+		$res=mysql_query($ins);
+		if($res){
+			sendmdp($forlife,$mdp);
+			$msg = "Compte mise à jour avec succès. Un mail a été envoyé à ton adresse $forlife@polytechnique.edu avec ton mot de passe. Tu peux retourner maintenant à la <a href=\"index.php\">page d'accueil</a>.";
+		}
+		else{
+			$msg = "Un problème est survenu et ton compte n'a pas pu être changé. Contacte un <a href=\"mailto:robotran@frankiz.polytechnique.fr\">administrateur du système</a>";
+		}
+	}
+}
+
+require BASE_LOCAL."/include/page_header.inc.php";
+?><page id='robotran' titre='Frankiz : Comptes Robotran'>
+<?php
+
+if(isset($errmsg) && $errmsg != ""){
+	echo("<warning>$errmsg</warning>\n");
+}
+?>
+
+<h1>Comptes Robotran</h1>
+Dans cette page tu peux créer un compte pour demander des codes pour les machines traditionnelles 
+de Robotran à Fayolle ou au PEM.
+<?php
+if(isset($msg)){
+	echo '<note>'.$msg.'</note>';
+}
+?>
+
+<note>Ton identifiant est la partie de ton adresse mail qui vient avant @polytechnique.edu. (En général, prenom.nom)</note>
+<formulaire id="createaccount" action="new_account.php" titre="Création de compte">
+  <champ id="forlife" titre="Identifiant"/>
+  <bouton id="envoyer" value="Continuer" titre="Créer"/>
+</formulaire>
+
+</page>
+<?php require_once BASE_LOCAL."/include/page_footer.inc.php" ?>
+
