ab7ed0601aa9e1287570b62a0e09662d5b1e582d
Brice Gelineau <brice.gelineau@polytechnique.edu>
2010-05-22 16:11:05 +0200

    added a minimodule for the IK
    Signed-off-by: Brice Gelineau <brice.gelineau@polytechnique.edu>

---
+++ minimodules/ik.php
@@ -0,0 +1,36 @@
+<?php
+/***************************************************************************
+ *  Copyright (C) 2009 Binet Réseau                                       *
+ *  http://www.polytechnique.fr/eleves/binets/reseau/                     *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the Free Software            *
+ *  Foundation, Inc.,                                                      *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ***************************************************************************/
+
+class IkMiniModule extends FrankizMiniModule
+{
+
+    public function __construct()
+    {
+        $res = XDB::query("SELECT * FROM ik ORDER BY date DESC LIMIT 1", $id)->fetchAllRow();
+        $this->assign('ik', $res[0]);
+        $this->tpl = "minimodules/ik/last_ik.tpl";
+        $this->titre = "IK électronique";
+    }
+
+}
+
+// vim:set et sw=4 sts=4 sws=4 foldmethod=marker enc=utf-8:
+?>
