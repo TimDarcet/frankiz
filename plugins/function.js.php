123c71e377f88b6c05f21127a930519e16c23c6f
Riton <henri.jouhaud@polytechnique.org>
2010-09-28 20:23:36 +0200

    To include a module's js, add {js src="myJs.js"} in the template
    Signed-off-by: Riton <henri.jouhaud@polytechnique.org>

---
+++ plugins/function.js.php
@@ -0,0 +1,32 @@
+<?php
+/***************************************************************************
+ *  Copyright (C) 2009 Binet Réseau                                       *
+ *  http://www.polytechnique.fr/eleves/binets/reseau/                     *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the Free Software            *
+ *  Foundation, Inc.,                                                      *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ***************************************************************************/
+
+/** This function will add a script tag to include a js file
+ * @param $src     Path to te file
+ */
+function smarty_function_js($params, &$smarty) {
+    $src = $params['src'];
+
+    return '<script type="text/javascript" src="' . pl_static_content_path('javascript/', $src) . '"></script>';
+}
+
+// vim:set et sw=4 sts=4 sws=4 foldmethod=marker enc=utf-8:
+?>
