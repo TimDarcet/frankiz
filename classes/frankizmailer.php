8a4071cc460d4aae21dd0dc37b3c6f2bdbba96fc
Brice <brice.gelineau@polytechnique.org>
2010-11-11 21:24:49 +0100

    corrected bug
    Signed-off-by: Brice <brice.gelineau@polytechnique.org>

--- classes/frankizmailer.php
+++ classes/frankizmailer.php
@@ -19,7 +19,6 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************/
 
-require_once('smarty/libs/Smarty.class.php');
 require_once('../include/class.phpmailer.php');
 
 class FrankizMailer extends PHPMailer
