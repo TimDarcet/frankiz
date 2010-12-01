5b325b74784ecc1fc576c48b61d6a12e70415848
Brice <brice.gelineau@polytechnique.org>
2010-12-01 09:49:08 +0100

    Handlers for the new proposals (also forgot this file)
    Signed-off-by: Brice <brice.gelineau@polytechnique.org>

--- modules/proposal.php
+++ modules/proposal.php
@@ -27,20 +27,22 @@ class ProposalModule extends PlModule
         return array(
             'proposal/news'             => $this->make_hook('news', AUTH_COOKIE),
             'proposal/activity'         => $this->make_hook('activity', AUTH_COOKIE),
+            'proposal/activity/ajax'    => $this->make_hook('activity_ajax', AUTH_COOKIE),
             'proposal/mail'             => $this->make_hook('mail', AUTH_COOKIE),
+            'proposal/qdj'              => $this->make_hook('qdj', AUTH_COOKIE),
         );
     }
 
     function handler_news($page)
     {   
         
-        $title      = env::t('title', '');
-        $content    = env::t('content', '');
-        $end        = env::t('end', '');
-        $comment    = env::t('comment', '');
-        $gid        = env::i('group_news_proposal', '');
-        if(env::t('origin_news_proposal') != '') $origin = env::i('origin');
-        $priv       = env::has('priv')?1:0;
+        $title      = Env::t('title', '');
+        $content    = Env::t('content', '');
+        $end        = Env::t('end', '');
+        $comment    = Env::t('comment', '');
+        $gid        = Env::i('group_news_proposal', '');
+        if(Env::t('origin_news_proposal') != '') $origin = Env::i('origin');
+        $priv       = Env::has('priv')?1:0;
         if ($end != '')
         {
             $end = substr_replace($end, '-', 6, 0);
@@ -48,8 +50,7 @@ class ProposalModule extends PlModule
         }
 
         $iid = 1; //To change as soon as Riton works
-
-        if (env::has('send'))
+        if (Env::has('send'))
         {
             if($title == '' || $content == '' || $end == '' || $gid == '')
             {
@@ -57,24 +58,32 @@ class ProposalModule extends PlModule
             }
             else 
             {
-                $n = new News(array(
-                    'writer'    => S::user(),
-                    'target'    => new Group($gid),
-                    'iid'       => $iid,
-                    'origin'    => new Group($origin),
-                    'title'     => $title,
-                    'content'   => $content,
-                    'end'       => $end,
-                    'comment'   => $comment,
-                    'priv'      => $priv));
-                $nv = new NewsValidate($n);
-                $v = new Validate(array(
-                    'user'  => S::user(),
-                    'gid'   => $gid,
-                    'item'  => $nv,
-                    'type'  => 'news'));
-                $v->insert();
-                $page->assign('envoye', true);
+                try
+                {
+                    $end_c = new FrankizDateTime($end);
+                    $n = new News(array(
+                        'writer'    => S::user(),
+                        'target'    => new Group($gid),
+                        'iid'       => $iid,
+                        'origin'    => new Group($origin),
+                        'title'     => $title,
+                        'content'   => $content,
+                        'end'       => $end_c,
+                        'comment'   => $comment,
+                        'priv'      => $priv));
+                    $nv = new NewsValidate($n);
+                    $v = new Validate(array(
+                        'user'  => S::user(),
+                        'gid'   => $gid,
+                        'item'  => $nv,
+                        'type'  => 'news'));
+                    $v->insert();
+                    $page->assign('envoye', true);
+                }
+                catch (Exception $e)
+                {
+                    $page->assign('msg', 'La date n\'est pas valide.');
+                }
             }
         }
 
@@ -92,15 +101,20 @@ class ProposalModule extends PlModule
 
     function handler_activity($page)
     {   
-        $title      = env::t('title', '');
-        $desc       = env::t('desc', '');
-        $date       = env::t('date', '');
-        $begin      = env::t('begin', '00:00');
-        $end        = env::t('end', '00:00');
-        $gid        = env::i('group_activity_proposal', '');
+        $title      = Env::t('title', '');
+        $desc       = Env::t('desc', '');
+        $date       = Env::t('date', '');
+        $begin      = Env::t('begin', '00:00');
+        $end        = Env::t('end', '00:00');
+        $target     = Env::i('target_activity_proposal', '');
+        if(Env::t('origin_activity_proposal') != '') $origin = Env::i('origin');
+        $origin     = Env::i('group_activity_proposal', '');
+        $priv       = Env::has('priv');
         
-        $number     = env::i('number', 1);
-        $regular    = env::has('regular')?1:0;
+        $c = new Collection('Activity');
+        $c->add(1);
+        $c->add(2);
+        $c->select(Activity::SELECT_BASE);
         
         if ($date != '')
         {
@@ -108,41 +122,119 @@ class ProposalModule extends PlModule
             $date = substr_replace($date, '-', 4, 0);
         }
         
-        // Do we want an image ?
-        $iid = 1; //To change as soon as Riton works
-        
-        if (env::has('send'))
+        if (Env::has('send_new'))
         {
             if($title == '' || $date == '' || $begin == '00:00' || $end == '00:00' 
-                || $gid == ''  || ($regular && ($number > 5 || $number < 1)))
+                || $target == '')
             {
-                $page->assign('msg', 'Il manque des informations pour créer l\'activité.');
+                $page->assign('msg', 'Il manque des informations pour créer l\'activité. 
+                    Attention les heures ne peuvent pas rester à 00:00.');
             }
             else 
             {
-                $av = new ActivityValidate(new Group($gid), $iid, $title, 
-                    $desc, $date, $begin, $end, $regular, $number);
-                $v = new Validate(array(
-                    'user'  => s::user(),
-                    'gid'   => $gid,
-                    'item'  => $av,
-                    'type'  => 'activity'));
-                $v->insert();
-                $page->assign('envoye', true);
+                try
+                {
+                    $date_c = new FrankizDateTime($date);
+                    $av = new ActivityValidate(s::user(), new Group($target), $title,
+                        $desc, $date_c, $begin, $end, $priv, is_null($origin)?null:new Group($origin));
+                    $v = new Validate(array(
+                        'user'  => S::user(),
+                        'gid'   => $target,
+                        'item'  => $av,
+                        'type'  => 'activity'));
+                    $v->insert();
+                    $page->assign('envoye', true);
+                }
+                catch (Exception $e)
+                {
+                    $page->assign('msg', 'La date est incorrecte.');
+                }
             }
         }
+        
+        if (Env::has('send_reg'))
+        {
+            $aid = Env::i('regular_activity_proposal', '');
+            $a = $c->get($aid);
+            if(!$a)
+            {
+                $page->assign('msg', 'Tu n\'as pas le droit de créer de nouvelles instances de cette activité.');
+            }
+            else 
+            {
+                $dates = $a->next_dates(5);
+		        foreach($dates as $temp)
+		            foreach($temp as $dat)
+		                if (Env::has($dat . '_regular_proposal'))
+		                {
+                            try
+                            {
+                                $begin_c = new FrankizDateTime($dat . ' ' . $begin . ':00');
+                                $end_c = new FrankizDateTime($dat . ' ' . $end . ':00');
+                                $av = new ActivityInstance(array(
+                                'aid'       => $aid,
+                                'writer'    => S::user(),
+                                'begin'     => $begin_c,
+                                'end'       => $end_c,
+                                'comment'   => Env::t('comment', '')));
+                                $av->replace();
+                            }
+                            catch (Exception $e)
+                            {
+                                $page->assign('msg', 'Les dates sont fausses.');
+                            }
+		                }
+                if (Env::has('other_regular_proposal'))
+                {
+                    try
+                    {
+                        $begin_c = new FrankizDateTime($date . ' ' . $begin . ':00');
+                        $end_c = new FrankizDateTime($date . ' ' . $end . ':00');
+                        $av = new ActivityInstance(array(
+                        'aid'       => $aid,
+                        'writer'    => S::user(),
+                        'begin'     => $begin_c,
+                        'end'       => $end_c,
+                        'comment'   => Env::t('comment', '')));
+                        $av->replace();
+                    }
+                    catch (Exception $e)
+                    {
+                        $page->assign('msg', 'Les dates sont fausses.');
+                    }
+                }
+                $page->assign('msg', 'Ton activité a été rajoutée.');
+            }
+        }
+        
+        if (Env::has('new_regular'))
+            pl_redirect('activity/regular/new');
+            
+        if (Env::has('modify_regular'))
+            pl_redirect('activity/regular/modify');
+            
         
         $page->assign('title_activity', $title);
         $page->assign('desc', $desc);
         $page->assign('date', $date);
-        $page->assign('regular', $regular);
-        $page->assign('number', $number);
         $page->assign('begin', $begin);
         $page->assign('end', $end);
+        $page->assign('priv', $priv);
         
+        $page->assign('regular_activities', $c);
         $page->assign('title', 'Proposer une activité');
         $page->addCssLink('validate.css');
         $page->changeTpl('validate/prop.activity.tpl');
+    }
+    
+    function handler_activity_ajax($page)
+    {   
+        $aid = Env::i('aid', '');
+        $a = new Activity($aid);
+        $a->select(Activity::SELECT_BASE);
+        $page->assign('days', $a->next_dates(5));
+        $page->assign('activity', $a);
+        $page->changeTpl('validate/prop.activity.ajax.tpl', NO_SKIN);
     }
     
     function handler_mail($page)
@@ -167,7 +259,7 @@ class ProposalModule extends PlModule
                 $el = new Validate(array(
                     'item'  =>$nv, 
                     'gid'   =>Env::t('group_mail_proposal'), 
-                    'user'  =>s::user(), 
+                    'user'  =>S::user(), 
                     'type'  =>'mail'));
                 $el->insert();
                 $page->assign('envoye', 1);
@@ -181,8 +273,35 @@ class ProposalModule extends PlModule
         $page->assign('title', 'Envoi des mails');
         $page->addCssLink('validate.css');
         $page->changeTpl('validate/prop.mail.tpl');
-    }
-}
+    }    
 
-// vim:set et sw=4 sts=4 sws=4 foldmethod=marker enc=utf-8:
-?>
+    function handler_qdj($page)
+    {   
+        $question = Env::t('quest');
+        $answer1 = Env::t('ans1');
+        $answer2 = Env::t('ans2');
+        
+        if (Env::has('send')) 
+        {
+            if($question == '' || $answer1 == '' || $answer2 == '')
+            {
+                $page->assign('msg', 'Il manque des informations.');
+            }
+            else 
+            {
+                $qv = new QDJValidate($question, $answer1, $answer2);
+                $v = new Validate(array(
+                    'user'  => S::user(),
+                    'gid'   => Group::from('qdj'),
+                    'item'  => $qv,
+                    'type'  => 'qdj'));
+                $v->insert();
+                $page->assign('envoye', true);
+            }
+        }
+        
+        $page->addCssLink('validate.css');
+        $page->assign('title', 'Proposition d\'une qdj');
+        $page->changeTpl('validate/prop.qdj.tpl');
+    }
+}    
