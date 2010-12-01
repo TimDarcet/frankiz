6fbd6046e7746000e02a5aa70518f7805228ebdf
Brice <brice.gelineau@polytechnique.org>
2010-12-01 09:56:40 +0100

    All the stuff for the calendar If someone has enough courage to clean wdcalendar.css, feel free
    Signed-off-by: Brice <brice.gelineau@polytechnique.org>

---
+++ htdocs/javascript/3.0.0beta/wdcalendar.js
@@ -0,0 +1,168 @@
+$(document).ready(function() {     
+           var view="week";          
+
+            $('#activity_show').hide();
+            $('#activity_show .first .close_show').click(function() {
+                $('#activity_show').hide();
+            });
+            $('#activity_show .msg').hide();
+
+
+            var DATA_FEED_URL = "activity/ajax/";
+            var op = {
+                view: view,
+                theme:3,
+                showday: new Date(),
+                ViewCmdhandler:View,    
+                onWeekOrMonthToDay:wtd,
+                onBeforeRequestData: cal_beforerequest,
+                onAfterRequestData: cal_afterrequest,
+                onRequestDataError: cal_onerror, 
+                autoload:true,
+                readonly:true,
+                url: DATA_FEED_URL + "/timetable"
+            };
+            var $dv = $("#calhead");
+            var _MH = document.documentElement.clientHeight;
+            var dvH = $dv.height() + 2;
+            op.height = _MH - dvH - 210;
+            //op.height = 600;
+            op.eventItems =[];
+
+            var p = $("#gridcontainer").bcalendar(op).BcalGetOp();
+            if (p && p.datestrshow) {
+                $("#txtdatetimeshow").text(p.datestrshow);
+            }
+            $("#caltoolbar").noSelect();
+            
+            $("#hdtxtshow").datepicker({ picker: "#txtdatetimeshow", showtarget: $("#txtdatetimeshow"),
+            onReturn:function(r){                          
+                            var p = $("#gridcontainer").gotoDate(r).BcalGetOp();
+                            if (p && p.datestrshow) {
+                                $("#txtdatetimeshow").text(p.datestrshow);
+                            }
+                     } 
+            });
+            function cal_beforerequest(type)
+            {
+                var t="Loading data...";
+                switch(type)
+                {
+                    case 1:
+                        t="Loading data...";
+                        break;
+                    case 2:                      
+                    case 3:  
+                    case 4:    
+                        t="The request is being processed ...";                                   
+                        break;
+                }
+                $("#errorpannel").hide();
+                $("#loadingpannel").html(t).show();    
+            }
+            function cal_afterrequest(type)
+            {
+                switch(type)
+                {
+                    case 1:
+                        $("#loadingpannel").hide();
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        $("#loadingpannel").html("Success!");
+                        window.setTimeout(function(){ $("#loadingpannel").hide();},2000);
+                    break;
+                }              
+               
+            }
+            function cal_onerror(type,data)
+            {
+                $("#errorpannel").show();
+            }
+
+            function View(data)
+            {
+                $('#activity_show').show()
+                load(new Array(data[0]), true);
+            }
+
+            function wtd(p)
+            {
+               if (p && p.datestrshow) {
+                    $("#txtdatetimeshow").text(p.datestrshow);
+                }
+                $("#caltoolbar div.fcurrent").each(function() {
+                    $(this).removeClass("fcurrent");
+                })
+                $("#showdaybtn").addClass("fcurrent");
+            }
+            //to show day view
+            $("#showdaybtn").click(function(e) {
+                //document.location.href="#day";
+                $("#caltoolbar div.fcurrent").each(function() {
+                    $(this).removeClass("fcurrent");
+                })
+                $(this).addClass("fcurrent");
+                var p = $("#gridcontainer").swtichView("day").BcalGetOp();
+                if (p && p.datestrshow) {
+                    $("#txtdatetimeshow").text(p.datestrshow);
+                }
+            });
+            //to show week view
+            $("#showweekbtn").click(function(e) {
+                //document.location.href="#week";
+                $("#caltoolbar div.fcurrent").each(function() {
+                    $(this).removeClass("fcurrent");
+                })
+                $(this).addClass("fcurrent");
+                var p = $("#gridcontainer").swtichView("week").BcalGetOp();
+                if (p && p.datestrshow) {
+                    $("#txtdatetimeshow").text(p.datestrshow);
+                }
+
+            });
+            //to show month view
+            $("#showmonthbtn").click(function(e) {
+                //document.location.href="#month";
+                $("#caltoolbar div.fcurrent").each(function() {
+                    $(this).removeClass("fcurrent");
+                })
+                $(this).addClass("fcurrent");
+                var p = $("#gridcontainer").swtichView("month").BcalGetOp();
+                if (p && p.datestrshow) {
+                    $("#txtdatetimeshow").text(p.datestrshow);
+                }
+            });
+            
+            $("#showreflashbtn").click(function(e){
+                $("#gridcontainer").reload();
+            });
+
+            //go to today
+            $("#showtodaybtn").click(function(e) {
+                var p = $("#gridcontainer").gotoDate().BcalGetOp();
+                if (p && p.datestrshow) {
+                    $("#txtdatetimeshow").text(p.datestrshow);
+                }
+
+
+            });
+            //previous date range
+            $("#sfprevbtn").click(function(e) {
+                var p = $("#gridcontainer").previousRange().BcalGetOp();
+                if (p && p.datestrshow) {
+                    $("#txtdatetimeshow").text(p.datestrshow);
+                }
+
+            });
+            //next date range
+            $("#sfnextbtn").click(function(e) {
+                var p = $("#gridcontainer").nextRange().BcalGetOp();
+                if (p && p.datestrshow) {
+                    $("#txtdatetimeshow").text(p.datestrshow);
+                }
+            });
+            
+        });
+
---
+++ templates/default/activity/timetable.tpl
@@ -0,0 +1,180 @@
+{**************************************************************************}
+{*                                                                        *}
+{*  Copyright (C) 2010 Binet Réseau                                       *}
+{*  http://www.polytechnique.fr/eleves/binets/reseau/                     *}
+{*                                                                        *}
+{*  This program is free software; you can redistribute it and/or modify  *}
+{*  it under the terms of the GNU General Public License as published by  *}
+{*  the Free Software Foundation; either version 2 of the License, or     *}
+{*  (at your option) any later version.                                   *}
+{*                                                                        *}
+{*  This program is distributed in the hope that it will be useful,       *}
+{*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *}
+{*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *}
+{*  GNU General Public License for more details.                          *}
+{*                                                                        *}
+{*  You should have received a copy of the GNU General Public License     *}
+{*  along with this program; if not, write to the Free Software           *}
+{*  Foundation, Inc.,                                                     *}
+{*  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA               *}
+{*                                                                        *}
+{**************************************************************************}
+
+{js src="plugins/datepicker_lang_FR.js"}
+{js src="plugins/wdCalendar_lang_FR.js"}
+
+{*don't understand why I have to put it again but don't work otherwise*}
+{js src="jquery.js"}
+{js src="plugins/Common.js"}
+{js src="plugins/jquery.datepicker.js"}
+{js src="plugins/jquery.alert.js"}
+{js src="plugins/jquery.ifrmdailog.js"}
+{js src="plugins/jquery.calendar.js"}
+
+
+{js src="activities.js"}
+{js src="wdcalendar.js"}
+
+
+<div>
+    <div id="calhead" style="padding-left:1px;padding-right:1px;">
+        <div id="loadingpannel" class="ptogtitle loadicon" style="display: none;">
+            Chargement des données...
+        </div>
+        <div id="errorpannel" class="ptogtitle loaderror" style="display: none;">
+            Désolé, les données n'ont pu être chargées, merci de réessayer plus tard
+        </div>
+    </div>
+    <div class="t1 chromeColor">
+        &nbsp;
+    </div>
+    <div class="t2 chromeColor">
+        &nbsp;
+    </div>
+    <div id="caltoolbar" class="ctoolbar">
+        <div id="showtodaybtn" class="fbutton">
+            <div>
+                <span title='Click to back to today ' class="showtoday">
+                    Aujourd'hui
+                </span>
+            </div>
+        </div>
+        <div class="btnseparator">
+        </div>
+        <div id="showdaybtn" class="fbutton">
+            <div>
+                <span title='Day' class="showdayview">
+                    Jour
+                </span>
+            </div>
+        </div>
+        <div  id="showweekbtn" class="fbutton fcurrent">
+            <div>
+                <span title='Week' class="showweekview">
+                    Semaine
+                </span>
+            </div>
+        </div>
+        <div  id="showmonthbtn" class="fbutton">
+            <div>
+                <span title='Month' class="showmonthview">
+                    Mois
+                </span>
+            </div>
+        </div>
+        <div class="btnseparator">
+        </div>
+        <div  id="showreflashbtn" class="fbutton">
+            <div>
+                <span title='Refresh view' class="showdayflash">
+                    Rafraîchir
+                </span>
+            </div>
+        </div>
+        <div class="btnseparator">
+        </div>
+        <div id="sfprevbtn" title="Prev"  class="fbutton">
+            <span class="fprev">
+            </span>
+        </div>
+        <div id="sfnextbtn" title="Next" class="fbutton">
+            <span class="fnext">
+            </span>
+        </div>
+        <div class="fshowdatep fbutton">
+            <div>
+                <input type="hidden" name="txtshow" id="hdtxtshow" />
+                <span id="txtdatetimeshow">
+                    Choisir une date
+                </span>
+            </div>
+        </div>
+        <div class="clear">
+        </div>
+    </div>
+    <div class="t2 chromeColor">
+        &nbsp;
+    </div>
+    <div class="t1 chromeColor">
+        &nbsp;
+    </div>
+</div>
+
+
+<div style="padding:1px;">
+
+    <div class="t1 chromeColor">
+        &nbsp;
+    </div>
+    <div class="t2 chromeColor">
+        &nbsp;
+    </div>
+    <div id="dvCalMain" class="calmain printborder">
+        <div id="gridcontainer" style="overflow-y: visible;">
+        </div>
+    </div>
+    <div class="t2 chromeColor">
+        &nbsp;
+    </div>
+    <div class="t1 chromeColor">
+        &nbsp;
+    </div>
+
+</div>
+
+<div id="activity_show" class="timetable">
+    <div class="first">
+        [<span class="target">
+        </span>]
+        <span class="title">
+        </span>
+        <span class="close_show">
+        [X]
+        </span>
+    </div>
+    <div class="padd">
+        <div class="date">
+        </div>
+        <div class="time">
+            de
+            <span class="hour_begin">
+            </span>
+            à
+            <span class="hour_end">
+            </span>
+        </div>
+        <div class="msg">
+        </div>
+        <div class="participate">
+        </div>
+        <div class="section">
+            Description :
+        </div>
+        <div class="description">
+        </div>
+        <div class="comment">
+        </div>
+    </div>
+</div>
+
+{* vim:set et sw=2 sts=2 sws=2 enc=utf-8: *}
