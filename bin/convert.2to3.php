9f2627a88e583bfa5b23070b4d6578e063b25aa3
Riton <riton@melix.net>
2011-01-12 01:25:10 +0100

    Added Table Account to the list of tables with defaults rows
    Signed-off-by: Riton <riton@melix.net>

--- bin/convert.2to3.php
+++ bin/convert.2to3.php
@@ -52,7 +52,7 @@ echo 'DB created ' . "\n";
 echo "-----------------------------------------------\n";
 
 // Import "static" tables
-$tables = array('frankiz', 'formations', 'days', 'ips', 'minimodules', 'rooms', 'skins');
+$tables = array('frankiz', 'account', 'formations', 'days', 'ips', 'minimodules', 'rooms', 'skins');
 foreach ($tables as $table) {
     echo exec('mysql -h ' . $globals->dbhost . ' -u ' . $globals->dbuser .
               ' -p' . $globals->dbpwd . ' ' . $globals->dbdb . ' < sql/' . $table . '.sql');
---
+++ bin/sql/account.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 12, 2011 at 12:28 AM
+-- Server version: 5.1.51
+-- PHP Version: 5.3.4-pl0-gentoo
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `dev`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE IF NOT EXISTS `account` (
+  `uid` int(10) NOT NULL AUTO_INCREMENT,
+  `hruid` varchar(255) CHARACTER SET ascii NOT NULL,
+  `password` varchar(120) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `gid` int(11) NOT NULL,
+  `perms` enum('admin','web','support','kes','none') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'none',
+  `state` enum('active','pending','unregistered','disabled') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `hash` varchar(34) CHARACTER SET ascii NOT NULL,
+  `hashstamp` datetime NOT NULL,
+  `hash_rss` varchar(34) CHARACTER SET ascii NOT NULL,
+  `email_format` enum('text','html') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'text',
+  `skin` varchar(40) NOT NULL,
+  `firstname` tinytext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` tinytext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `nickname` tinytext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `bestalias` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `gender` enum('man','woman') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'man',
+  `birthdate` date NOT NULL DEFAULT '0000-00-00',
+  `next_birthday` date NOT NULL DEFAULT '0000-00-00',
+  `cellphone` char(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `original` int(11) NOT NULL DEFAULT '0',
+  `photo` int(11) NOT NULL DEFAULT '0',
+  `comment` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `on_platal` tinyint(1) NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `hruid` (`hruid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10319 ;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`uid`, `hruid`, `password`, `gid`, `perms`, `state`, `hash`, `hashstamp`, `hash_rss`, `email_format`, `skin`, `firstname`, `lastname`, `nickname`, `bestalias`, `gender`, `birthdate`, `next_birthday`, `cellphone`, `original`, `photo`, `comment`, `on_platal`) VALUES
+(0, 'anonymous', '', 0, 'none', 'disabled', '', '0000-00-00 00:00:00', '', 'text', 'default', '', '', '', '', 'man', '0000-00-00', '0000-00-00', '', 0, 0, 'Anonymous User', 0);
