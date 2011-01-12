e593b6b4a1a27afd3910414b287b859062350bc8
Riton <riton@melix.net>
2011-01-12 02:16:34 +0100

    Minor BugFix
    Signed-off-by: Riton <riton@melix.net>

--- classes/frankizpage.php
+++ classes/frankizpage.php
@@ -19,8 +19,6 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************/
 
-require_once '../include/smartphone.inc.php';
-
 class SkinFileNotFoundException extends Exception
 {
     protected $skin;
--- include/common.inc.php
+++ include/common.inc.php
@@ -19,6 +19,8 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************/
 
+require_once 'smartphone.inc.php';
+
 function __autoload($cls)
 {
     if (!pl_autoload($cls)) {
