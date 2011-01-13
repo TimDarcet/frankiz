c302eb0644e3f06cc7a5707c3325a25f815d2794
Riton <riton@melix.net>
2011-01-13 02:44:09 +0100

    Added formations: master, doctorants & PEI
    Signed-off-by: Riton <riton@melix.net>

--- bin/sql/formations.sql
+++ bin/sql/formations.sql
@@ -40,5 +40,9 @@ CREATE TABLE IF NOT EXISTS `formations` (
 
 INSERT INTO `formations` (`formation_id`, `domain`, `title`, `abbrev`) VALUES
 (0, 'frankiz.net', 'Frankiz', 'fkz'),
-(1, 'polytechnique.edu', 'Polytechnique', 'x'),
-(2, 'poly.polytechnique.fr', 'Poly', 'poly');
+(1, 'polytechnique.edu', 'Polytechnicien', 'x'),
+(2, 'poly.polytechnique.fr', 'Anciens comptes', 'poly'),
+(3, 'polytechnique.edu', 'Master de l\'X', 'master'),
+(4, 'polytechnique.edu', 'Doctorants de l\'X', 'doc'),
+(5, 'polytechnique.edu', 'PEI', 'pei');
+;
