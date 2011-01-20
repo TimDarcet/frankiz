ae1064fd69978730b3a4df71e416b87d0eed7403
Riton <riton@melix.net>
2011-01-21 00:14:43 +0100

    Added Nix.decode() to decode emails after a str13
    Signed-off-by: Riton <riton@melix.net>

--- htdocs/javascript/3.0.0beta/common.js
+++ htdocs/javascript/3.0.0beta/common.js
@@ -9,7 +9,6 @@
    };
 })(jQuery);
 
-var currentPage = '';
 $(document).ready(function(){
     $.ajaxSetup({ cache: false });
 });
@@ -254,4 +253,30 @@ function groups_picker(id, ns, check)
         newsearch = true;
         search();
     });
+}
+
+Nix = {
+    map: null,
+    convert: function(a) {
+        Nix.init();
+        var s = '';
+        for (i = 0; i < a.length ; i++) {
+            var b = a.charAt(i);
+            s += ((b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z') ? Nix.map[b] : b);
+        }
+        return s;
+    },
+    init: function() {
+              if (Nix.map != null)
+                  return;
+              var map = new Array();
+              var s='abcdefghijklmnopqrstuvwxyz';
+              for (i = 0; i < s.length; i++)
+                  map[s.charAt(i)] = s.charAt((i+13)%26);
+              for (i=0; i<s.length; i++)map[s.charAt(i).toUpperCase()] = s.charAt((i+13)%26).toUpperCase();
+              Nix.map = map;
+          },
+    decode: function(a) {
+                document.write(Nix.convert(a));
+            }
 }
