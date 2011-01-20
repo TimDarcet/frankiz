42bf0a0dff3ace0d8ae16517f5be3a9a558bf29c
Riton <riton@melix.net>
2011-01-20 16:37:27 +0100

    Move groups_picker() JS to common.js
    Removed old code from groups.js
    
    Signed-off-by: Riton <riton@melix.net>

--- htdocs/javascript/3.0.0beta/common.js
+++ htdocs/javascript/3.0.0beta/common.js
@@ -117,3 +117,141 @@ function wikify(container)
         wiki_preview.start(textarea, display);
     });
 }
+
+function groups_picker(id, ns, check)
+{
+    // Principal blocks
+    var container  = $("#groups_picker_" + id);
+    var selected   = container.children(".selected").first();
+    var list       = container.children(".list").first();
+    var searcher   = container.children(".searcher").first();
+    var filter     = searcher.children("[name=filter]").first();
+    var input      = $("#" + id);
+
+    var searching = false;
+    var newsearch = false;
+    var focus     = false;
+
+    // What blocks are to be hidden / shown
+    input.hide();
+    searcher.hide();
+    list.hide();
+    var handler = function() {
+        list.slideUp(100);
+        searcher.slideUp(100);
+        focus = false;
+    };
+    container.click(function() {
+        if (!focus) {
+            searcher.show();
+            filter.val('');
+            filter.focus();
+            focus = true;
+            search();
+        }
+    });
+    container.mouseleave(function() {
+      $(document).mouseup(handler);
+    });
+    container.mouseenter(function() {
+      $(document).unbind("mouseup", handler);
+    });
+
+    var fillInput = function (sendEvent)
+    {
+        sendEvent = typeof(sendEvent) != 'undefined' ? sendEvent : true;
+
+        var selecteds = selected.children('li');
+        if (selecteds.length > 0)
+            container.removeClass('empty');
+        else
+            container.addClass('empty');
+
+        var gids = new Array();
+        selected.children('li').each(function() {
+            if ($(this).attr('gid'))
+                gids.push($(this).attr('gid'));
+        });
+
+        input.val(gids.join(';'));
+
+        if (sendEvent) {
+            input.keyup();
+        }
+    };
+
+    selected.children('li').click(function() {
+        if (focus) {
+            $(this).remove();
+            fillInput();
+        }
+    });
+
+    fillInput(false);
+
+    // Search logic
+    var search = function ()
+    {
+        if (!searching) {
+            newsearch = false;
+            searching = true;
+            container.addClass('searching');
+            request({
+                "url": 'groups/ajax/search'
+              ,"data": {"ns": ns, "token" : filter.val()}
+              ,"fail": false
+           ,"success": function(json) {
+                           list.empty();
+                           var groups = json.groups;
+                           groups.sort(function(a, b) {
+                               return (a.frequency < b.frequency) ? -1 : (a.frequency > b.frequency) ? 1 : 0;
+                            });
+                           var group;
+                           for (var i in groups)
+                           {
+                               group = json.groups[i];
+                               list.append('<li gid="' + group.id + '">' +
+                                               '<img src="' + group.src + '" />' +
+                                               group.label +
+                                           '</li>');
+                           }
+
+                           list.children('li').click(function() {
+                               var gid = $(this).attr('gid');
+                               var alreadyExists = false;
+                               selected.children('li').each(function() {
+                                   if ($(this).attr('gid') == gid) {
+                                       alreadyExists = true;
+                                       return false;
+                                   }
+                               });
+                               if (!alreadyExists)
+                               {
+                                   var sel = $('<li gid="' + $(this).attr('gid') + '">' + $(this).html() + '</li>');
+                                   sel.appendTo(selected);
+                                   sel.click(function() {
+                                       if (focus) {
+                                           $(this).remove();
+                                           fillInput();
+                                       }
+                                   });
+                                   fillInput();
+                               }
+                           });
+
+                           list.slideDown(100);
+
+                           searching = false;
+                           container.removeClass('searching');
+                           if (newsearch)
+                               search();
+                       }
+            });
+        }
+    };
+
+    filter.keyup(function() {
+        newsearch = true;
+        search();
+    });
+}
