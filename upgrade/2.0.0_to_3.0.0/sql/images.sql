50a5da189530f55df239ca218c381d58e9cfd1cd
Riton <riton@melix.net>
2011-01-20 16:24:58 +0100

    BugFix in the schema of the table 'images'
    Signed-off-by: Riton <riton@melix.net>

--- upgrade/2.0.0_to_3.0.0/sql/images.sql
+++ upgrade/2.0.0_to_3.0.0/sql/images.sql
@@ -3,17 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 19, 2011 at 10:14 PM
+-- Generation Time: Jan 20, 2011 at 04:06 PM
 -- Server version: 5.1.51
 -- PHP Version: 5.3.5-pl0-gentoo
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: 'dev'
@@ -26,7 +20,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 DROP TABLE IF EXISTS images;
-CREATE TABLE images (
+CREATE TABLE IF NOT EXISTS images (
   iid int(11) NOT NULL AUTO_INCREMENT,
   gid int(11) NOT NULL,
   mime varchar(25) CHARACTER SET utf8 NOT NULL,
@@ -34,10 +28,11 @@ CREATE TABLE images (
   y smallint(5) unsigned NOT NULL,
   label varchar(200) CHARACTER SET utf8 DEFAULT NULL,
   description text CHARACTER SET utf8 NOT NULL,
-  micro blob NOT NULL,
+  micro blob,
   small blob,
   `full` mediumblob NOT NULL,
   seen int(11) NOT NULL,
   lastseen datetime NOT NULL,
   PRIMARY KEY (iid)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
