37c5ff81ea2797706fb357725f35ee659f736582
Riton <riton@melix.net>
2011-02-05 18:52:44 +0100

    Added table Links
    Signed-off-by: Riton <riton@melix.net>

---
+++ upgrade/2.0.0_to_3.0.0/sql/10_links.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS links;
+CREATE TABLE IF NOT EXISTS links (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  image int(11) DEFAULT NULL,
+  link tinytext NOT NULL,
+  label tinytext NOT NULL,
+  description text NOT NULL,
+  `comment` text NOT NULL,
+  ns enum('partners','usefuls') NOT NULL,
+  rank int(11) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+INSERT INTO links (id, image, link, `label`, description, `comment`, ns, rank) VALUES(1, NULL, 'home/contact', 'Contacter les élèves', '', '', 'usefuls', 0);
+INSERT INTO links (id, image, link, `label`, description, `comment`, ns, rank) VALUES(2, NULL, 'home/howtocome', 'Venir à l\'X', '', '', 'usefuls', 1);
+INSERT INTO links (id, image, link, `label`, description, `comment`, ns, rank) VALUES(3, NULL, 'links/partners', 'Partenariats', '', '', 'usefuls', 2);
+INSERT INTO links (id, image, link, `label`, description, `comment`, ns, rank) VALUES(4, NULL, 'http://www.polytechnique.edu', 'Site de l\'École', '', '', 'usefuls', 3);
+INSERT INTO links (id, image, link, `label`, description, `comment`, ns, rank) VALUES(5, NULL, 'ttp://www.etudes.polytechnique.edu', 'Site de la DE', '', '', 'usefuls', 4);
+INSERT INTO links (id, image, link, `label`, description, `comment`, ns, rank) VALUES(6, NULL, 'http://enex.polytechnique.fr', 'ENEX', '', '', 'usefuls', 5);
+INSERT INTO links (id, image, link, `label`, description, `comment`, ns, rank) VALUES(7, NULL, 'http://www.polytechnique.fr/sites/orientation4a/pages_orientation/', 'Orientation 4eme année', '', '', 'usefuls', 6);
