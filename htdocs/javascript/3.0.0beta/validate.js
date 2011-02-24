6592069af5b5b1a59538cf8c23519402a0fd2266
Riton <riton@melix.net>
2011-02-24 21:48:47 +0100

    Cleaned-up validate's js
    Signed-off-by: Riton <riton@melix.net>

--- htdocs/javascript/3.0.0beta/validate.js
+++ htdocs/javascript/3.0.0beta/validate.js
@@ -4,79 +4,70 @@ $(function() {
         $(this).siblings(".more").toggle();
     });
 
-	// For validations
-	$(".hide").hide();
-	$(".click").click(function() {
-		$(this).next(".hide").slideToggle(0);
-	});
-	$(".show").show();
-	
-	$(".addcom_validate").hide();
-	$(".text_validate").height(15);
-	$(".text_validate").click(function() {
-		$(".addcom_validate").slideToggle(150);
-		if ($(".text_validate").height() == 80)
-			$(".text_validate").height(15);
-		else
-			$(".text_validate").height(80);
-	});
+    // For validations
+    $(".hide").hide();
+    $(".click").click(function() {
+        $(this).next(".hide").slideToggle(0);
+    });
+    $(".show").show();
 
-	$("input[type=submit][name=refuse]").click(function() {
-	    if (!$(this).closest("li.validate").find("textarea[name=ans]").val()) {
-	        alert("Il manque une raison au refus");
-	        return false;
-	    }
-	});
+    $(".addcom_validate").hide();
+    $(".text_validate").height(15);
+    $(".text_validate").click(function() {
+        $(".addcom_validate").slideToggle(150);
+        if ($(".text_validate").height() == 80)
+            $(".text_validate").height(15);
+        else
+            $(".text_validate").height(80);
+    });
 
-	// For propositions
-	// mails
-	$("#no_preview").click(function() {
-		if ($(this).attr('checked')) {
-            wiki_preview.stop($('#text_proposal'));
-			$("#preview_proposal").html('Tu as choisi de ne pas formatter ton texte. L\'aperçu est donc désactivé.');
+    $("input[type=submit][name=refuse]").click(function() {
+        if (!$(this).closest("li.validate").find("textarea[name=ans]").val()) {
+            alert("Il manque une raison au refus");
+            return false;
         }
-		else
-			wiki_preview.start($('#text_proposal'), $('#preview_proposal'));
-	});
-	wiki_preview.start($('#text_proposal'), $('#preview_proposal'));
+    });
 
-	// activities
-	$("input[name='regular_activity_proposal']").change(function(){
-	    if ($("input[@name='regular_activity_proposal']:checked").val() == '0')
-	    {
-	    	$("#new_activity_proposal").show();
-	    	$("#old_activity_proposal").html('');
-	    	$("#old_activity_proposal").hide();
-	    }
-	    else 
-	    {
-	    	$("#new_activity_proposal").hide();
-	    	$("#old_activity_proposal").show();
-	    	$.ajax({
+    // For propositions
+    // mails
+    $("#no_preview").click(function() {
+        if ($(this).attr('checked')) {
+            wiki_preview.stop($('#text_proposal'));
+            $("#preview_proposal").html("Tu as choisi de ne pas formatter ton texte. L'aperçu est donc désactivé.");
+        }
+        else
+            wiki_preview.start($('#text_proposal'), $('#preview_proposal'));
+    });
+    wiki_preview.start($('#text_proposal'), $('#preview_proposal'));
+
+    // activities
+    $("input[name='regular_activity_proposal']").change(function(){
+        if ($("input[@name='regular_activity_proposal']:checked").val() == '0')
+        {
+            $("#new_activity_proposal").show();
+            $("#old_activity_proposal").html('');
+            $("#old_activity_proposal").hide();
+        }
+        else        {
+            $("#new_activity_proposal").hide();
+            $("#old_activity_proposal").show();
+            $.ajax({
                 type: 'POST',
                  url: 'proposal/activity/ajax',
                 data: 'aid=' + $("input[@name='regular_activity_proposal']:checked").val(),
              success: function(data) { $("#old_activity_proposal").html(data); }
           });
-	    }
-	});
-	
-	
-	
-	if ($('#regular_activity_proposal').attr('checked'))
-		$("#number_activity_proposal").show();
-	else
-		$("#number_activity_proposal").hide();
-	$("#regular_activity_proposal").click(function() {
-		if ($(this).attr('checked'))
-			$("#number_activity_proposal").show();
-		else
-			$("#number_activity_proposal").hide();
-	});
-	
-	// qdj
-	wiki_preview.start($('#quest_qdj_proposal'), $('.question'));
-	wiki_preview.start($('#ans1_qdj_proposal'), $('.answer1'));
-	wiki_preview.start($('#ans2_qdj_proposal'), $('.answer2'));
+        }
+    });
 
+    if ($('#regular_activity_proposal').attr('checked'))
+        $("#number_activity_proposal").show();
+    else
+        $("#number_activity_proposal").hide();
+    $("#regular_activity_proposal").click(function() {
+        if ($(this).attr('checked'))
+            $("#number_activity_proposal").show();
+        else
+            $("#number_activity_proposal").hide();
+    });
 });
