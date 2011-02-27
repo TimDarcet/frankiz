1b7244ac85c7a3f58148b0a43133803d760a87f0
Riton <riton@melix.net>
2011-02-27 15:05:44 +0100

    Updated MiniModules layout for non-logged users
    Signed-off-by: Riton <riton@melix.net>

--- upgrade/2.0.0_to_3.0.0/sql/20_anonymous_datas.sql
+++ upgrade/2.0.0_to_3.0.0/sql/20_anonymous_datas.sql
@@ -4,13 +4,14 @@ INSERT INTO account (uid, hruid, `password`, `group`, perms, state, `hash`, hash
 INSERT INTO account (uid, hruid, `password`, `group`, perms, state, `hash`, hashstamp, hash_rss, email_format, skin, firstname, lastname, nickname, email, gender, birthdate, next_birthday, cellphone, original, photo, `comment`) VALUES('0', 'anonymous.internal', '', 0, 'anonymous', 'disabled', '', '0000-00-00 00:00:00', '', 'text', 'default', '', '', '', '', 'woman', '0000-00-00', '0000-00-00', '', 0, 0, 'Internal anonymous User');
 
 
-INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'days', 'COL_LEFT', 0);
-INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'ik', 'COL_LEFT', 1);
-INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'meteo', 'COL_MIDDLE', 0);
-INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'jtx', 'COL_RIGHT', 1);
+INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'birthday', 'COL_LEFT', 0);
+INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'days', 'COL_LEFT', 1);
+INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'jtx', 'COL_MIDDLE', 0);
+INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'ik', 'COL_RIGHT', 0);
 INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'quicksearch', 'COL_FLOAT', 0);
+INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(0, 'links', 'COL_FLOAT', 1);
 
 
-INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(-1, 'days', 'COL_FLOAT', 0);
-INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(-1, 'meteo', 'COL_FLOAT', 1);
-INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(-1, 'timeleft', 'COL_FLOAT', 2);
+INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(-1, 'links', 'COL_FLOAT', 0);
+INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(-1, 'days', 'COL_FLOAT', 1);
+INSERT INTO users_minimodules (uid, `name`, col, `row`) VALUES(-1, 'meteo', 'COL_FLOAT', 2);
