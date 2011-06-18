b284ecc95695debecf1d6eb22dca2d1f9c60ebed
Matthieu Chareton <matthieu.chareton@polytechnique.org>
2011-06-19 00:46:46 +0200

    minimodule rer

---
+++ minimodules/rer.php
@@ -0,0 +1,57 @@
+<?php
+/***************************************************************************
+ *  Copyright (C) 2009 Binet Réseau                                       *
+ *  http://www.polytechnique.fr/eleves/binets/reseau/                     *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the Free Software            *
+ *  Foundation, Inc.,                                                      *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ***************************************************************************/
+
+class RerMiniModule extends FrankizMiniModule
+{
+    public function auth()
+    {
+        return AUTH_COOKIE;
+    }
+
+    public function perms()
+    {
+        return 'user';
+    }
+
+    public function tpl()
+    {
+        return 'minimodules/rer/rer.tpl';
+    }
+    
+    public function css()
+    {
+        return 'minimodules/rer.css';
+    }
+    
+    public function title()
+    {
+        return 'Prochains RER';
+    }
+
+    public function run()
+    {
+        if($xml = simplexml_load_string(RerLoader::get()))
+            $this->assign('trains',$xml->schedules->schedule->liste->children());
+    }
+}
+
+// vim:set et sw=4 sts=4 sws=4 foldmethod=marker enc=utf-8:
+?>
