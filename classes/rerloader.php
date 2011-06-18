29ff110a07d2064c5da8c478ed6834f6bf494755
Matthieu Chareton <matthieu.chareton@polytechnique.org>
2011-06-19 00:34:42 +0200

    minimodule rer

---
+++ classes/rerloader.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************************
+ *  Copyright (C) 2009 Binet Réseau                                        *
+ *  http://www.polytechnique.fr/eleves/binets/reseau/                      *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the Free Software            *
+ *  Foundation, Inc.,                                                      *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ***************************************************************************/
+
+class RerLoader
+{
+    static $urls = array(
+        'toParis'   => "http://ratp-bridge.fabernovel.com/ratp.schedule?reseau=4&direction=52&station=881&%20H",
+        'toStRemy'  => "http://ratp-bridge.fabernovel.com/ratp.schedule?reseau=4&direction=55&station=836&%20H"
+    );
+    
+    private static function response($destination){
+        $api = new API(self::$urls[$destination]);
+        return utf8_decode($api->response());
+    }
+    
+    public static function get($destination = 'toParis') {
+        global $globals;
+
+        if (!PlCache::hasGlobal('rer_' . $destination))
+            PlCache::setGlobal('rer_' . $destination, self::response($destination), $globals->cache->rer);
+
+        return PlCache::getGlobal('rer_' . $destination);
+    }
+}
+
+// vim:set et sw=4 sts=4 sws=4 foldmethod=marker enc=utf-8:
+?>
