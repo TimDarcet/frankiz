3768b100d773ac6810eaeb5579d42d5e9c2fccc1
Cyprien Mangin <cyprien.mangin@m4x.org>
2012-05-16 15:20:06 +0200

    Created bin/events_scripts
    Signed-off-by: Cyprien Mangin <cyprien.mangin@m4x.org>

---
+++ bin/events_scripts/minimodule.addtoall.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php -q
+<?php
+/***************************************************************************
+ *  Copyright (C) 2012 Binet Réseau                                        *
+ *  http://www.polytechnique.fr/eleves/binets/reseau/                      *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the Free Software            *
+ *  Foundation, Inc.,                                                      *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ***************************************************************************/
+/**
+ * Activate timeleft inimodue for every user
+ */
+
+require_once(dirname(__FILE__) . '/../connect.db.inc.php');
+
+// Get all users
+$userfilter = new UserFilter(new UFC_Group(Group::from('on_platal')));
+$users = $userfilter->get();
+$users->select(UserSelect::minimodules());
+$users->select(UserSelect::base());
+
+// Get timeleft minimodules
+$m = FrankizMinimodule::get($argv[1]);
+
+foreach ($users as $u) {
+    $minimodules = $u->minimodules();
+    if (!in_array($argv[1], $minimodules)) {
+        echo 'Adding minimodule to ' . $u->login() . PHP_EOL;
+        $u->addMinimodule($m);
+    }
+}
+?>
