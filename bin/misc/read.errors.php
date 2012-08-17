a25aeda4cd1be6bc0b473c848bae2db5a0824f7a
Nicolas Iooss <nicolas.iooss@polytechnique.org>
2012-08-17 19:38:17 -0400

    Add a script to read site_errors from command line
    Signed-off-by: Nicolas Iooss <nicolas.iooss@polytechnique.org>

---
+++ bin/misc/read.errors.php
@@ -0,0 +1,34 @@
+#!/usr/bin/php -q
+<?php
+/***************************************************************************
+ *  Copyright (C) 2004-2012 Binet Réseau                                   *
+ *  http://br.binets.fr/                                                   *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the Free Software            *
+ *  Foundation, Inc.,                                                      *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ***************************************************************************/
+
+/**
+ * Print site errors
+ */
+require_once(dirname(__FILE__) . '/../connect.db.inc.php');
+$errors = PlErrorReport::iterate();
+while (($err = $errors->next()) !== null) {
+    echo "------------ " . $err->date . " ------------\n";
+    if (!empty($err->state['Env']['n'])) {
+        echo "Requested page: " . $err->state['Env']['n'] . "\n";
+    }
+    echo $err->error . "\n";
+}
