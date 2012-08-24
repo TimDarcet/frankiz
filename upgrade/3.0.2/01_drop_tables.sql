3bc958e5e2043cf6817ba065636a10c8f2ad73b4
Nicolas Iooss <nicolas.iooss@polytechnique.org>
2012-08-24 12:05:06 -0400

    Drop unused tables
    Signed-off-by: Nicolas Iooss <nicolas.iooss@polytechnique.org>

---
+++ upgrade/3.0.2/01_drop_tables.sql
@@ -0,0 +1,11 @@
+--
+-- Drop no longer used tables
+--
+DROP TABLE IF EXISTS `brs_profiles`;
+DROP TABLE IF EXISTS `brs_subs`;
+DROP TABLE IF EXISTS `requests`;
+DROP TABLE IF EXISTS `requests_answers`;
+DROP TABLE IF EXISTS `rooms_essai`;
+DROP TABLE IF EXISTS `trombino_master`;
+DROP TABLE IF EXISTS `trombino_x`;
+
