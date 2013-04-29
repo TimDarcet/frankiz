a395f5c3a50bf1df52c53416dfe04311f7fa222c
Levans <victor.berger@polytechnique.org>
2013-04-29 18:08:28 +0200

    Changing scripts for forcing skins.

---
+++ bin/events_scripts/skin.forcetogroup.php
@@ -0,0 +1,39 @@
+#!/usr/bin/php -q
+<?php
+/***************************************************************************
+ *  Copyright (C) 2004-2013 Binet Réseau                                   *
+ *  http://br.binets.fr/                                                   *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the Free Software            *
+ *  Foundation, Inc.,                                                      *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ***************************************************************************/
+/**
+/**
+ * Force a skin to every user in groupe on_platal
+ */
+
+require_once(dirname(__FILE__) . '/../connect.db.inc.php');
+
+// Get all users
+$userfilter = new UserFilter(new UFC_Group(Group::from($argv[1])));
+$users = $userfilter->get();
+$users->select(UserSelect::base());
+
+$skin = empty($argv[1])?'default':$argv[2];
+
+foreach ($users as $u) {
+    echo 'Forcing skin to ' . $u->login() . PHP_EOL;
+    $u->skin($skin);
+}
